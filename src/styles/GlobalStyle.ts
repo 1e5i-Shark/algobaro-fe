f246ca47216cc4c64a2d145646b52f86c1cdfd21
Suyoung Shin <joyswim.dev@gmail.com>
2024-02-13 23:00:33 +0900

    [🎨style]: GlobalStyles 전역 스타일 지정

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,20 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    color: ${props => props.theme.text_primary_color};
+  }
+  body {
+    background-color: ${props => props.theme.background_color};
+  }
+`;
+
+export const Col = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
