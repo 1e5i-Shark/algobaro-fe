573588d5ea3a2031185aed7d9c60ba045ba5d368
Suyoung Shin <joyswim.dev@gmail.com>
2024-02-13 22:23:04 +0900

    [🎨style]: theme 다크, 라이트 모드 색상 지정

---
+++ src/styles/theme.ts
@@ -0,0 +1,48 @@
+export const lightTheme = {
+  mode: 'light',
+  background_primary: '#FFFFFF',
+  background_second: '#E9E9E9',
+  primary_color: '#A5F1F6',
+  secondary_color: '#CFBEFF',
+  black_primary: '#262626',
+  white_primary: '#FAFAFA',
+  text_primary_color: '#2C2C2C',
+  text_secondary_color: '#F0F0F0',
+  gray_50: '#909090',
+  gray_30: '#B3B3B3',
+  gray_20: '#E3E3E3',
+  gray_10: '#EFEFEF',
+  transparent_10: '#B3B3B310',
+  transparent_30: '#B3B3B330',
+  transparent_50: '#B3B3B350',
+  transparent_90: '#B3B3B390',
+  red: '#FF7C60',
+};
+
+export const darkTheme = {
+  mode: 'dark',
+  background_color: '#1E1B1A',
+  background_second: '#131010',
+  primary_color: '#A5F1F6',
+  secondary_color: '#CFBEFF',
+  black_primary: '#262626',
+  white_primary: '#FAFAFA',
+  text_primary_color: '#F0F0F0',
+  text_secondary_color: '#2C2C2C',
+  gray_50: '#909090',
+  gray_30: '#B3B3B3',
+  gray_20: '#E3E3E3',
+  gray_10: '#EFEFEF',
+  transparent_10: '#FEFEFE10',
+  transparent_30: '#FEFEFE30',
+  transparent_50: '#FEFEFE50',
+  transparent_90: '#FEFEFE90',
+  red: '#FF7C60',
+};
+
+const theme = {
+  lightTheme,
+  darkTheme,
+};
+
+export default theme;
