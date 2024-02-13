9c3ca2d35fd62fb664230383e9dbcf5f7a484215
Suyoung Shin <joyswim.dev@gmail.com>
2024-02-14 00:27:31 +0900

    [✨feat]: 로컬스토리지, 시스템에 따라 테마 값을 변경하는 ThemeContext 구현

---
+++ src/components/ThemeContext.tsx
@@ -0,0 +1,28 @@
+import { ReactNode, useMemo } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import useLocalStorage from '@/hooks/useLocalStorage';
+import { darkTheme, lightTheme } from '@/styles/theme';
+
+interface ThemeProps {
+  children: ReactNode;
+}
+
+export default function ThemeContext({ children }: ThemeProps) {
+  const [localTheme] = useLocalStorage('theme');
+
+  const themeMode = useMemo(() => {
+    if (localTheme) return localTheme;
+
+    const systemTheme = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches;
+    return systemTheme ? 'light' : 'dark';
+  }, [localTheme]);
+
+  return (
+    <ThemeProvider theme={themeMode === 'light' ? lightTheme : darkTheme}>
+      {children}
+    </ThemeProvider>
+  );
+}
