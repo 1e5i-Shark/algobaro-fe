57ad477c46d9b76b397eb706ec44fb07886e834e
Suyoung Shin <joyswim.dev@gmail.com>
2024-02-14 13:02:37 +0900

    [♻️refactor]: 필요 없는 변수 삭제

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -2,6 +2,5 @@ import { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
 
 export const useTheme = () => {
-  const themeContext = useContext(ThemeContext);
-  return themeContext;
+  return useContext(ThemeContext);
 };
