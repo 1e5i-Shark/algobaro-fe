19fed7e8c646eb250f99b5c5ee6667e4b434f5e0
Cho-Ik-Jun <harry960629@naver.com>
2024-02-15 12:58:40 +0900

    [🎨design]: background_modal 배경색 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,7 @@ export const lightTheme = {
   mode: 'light',
   background_primary: '#FFFFFF',
   background_second: '#E9E9E9',
+  background_modal: '#ffffff',
   primary_color: '#A5F1F6',
   secondary_color: '#CFBEFF',
   black_primary: '#262626',
@@ -21,8 +22,9 @@ export const lightTheme = {
 
 export const darkTheme = {
   mode: 'dark',
-  background_color: '#1E1B1A',
+  background_primary: '#1E1B1A',
   background_second: '#131010',
+  background_modal: '#1e1e1e',
   primary_color: '#A5F1F6',
   secondary_color: '#CFBEFF',
   black_primary: '#262626',
