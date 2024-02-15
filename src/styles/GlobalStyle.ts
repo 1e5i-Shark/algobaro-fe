74b7b7cfa59aa9dd0dfe7a8df6b75674e5e09045
pySoo <soopy368@gmail.com>
2024-02-15 11:41:14 +0900

    [🎨design]: global css 적용

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -2,13 +2,45 @@ import styled, { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   * {
+    box-sizing: border-box;
+    margin: 0;
     color: ${props => props.theme.text_primary_color};
   }
+
   html {
     font-size: 62.5%;
   }
+  
   body {
+    font-size: 1.6rem;
     background-color: ${props => props.theme.background_color};
+  }
+  
+  ul, ol, li {
+    padding: 0;
+    list-style: none;
+  }
+
+  input {
+    border: none;
+    outline: none;
+  }
+
+  textarea {
+    resize: none;
+    border: none;
+    outline: none;
+  }
+
+  button {
+    cursor: pointer;
+    background-color: transparent;
+    border: none;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
   }
 `;
 
