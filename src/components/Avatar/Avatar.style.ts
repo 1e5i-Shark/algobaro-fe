cc34d1100b1bad8bfe6f315f044e07019661acce
Suyoung Shin <joyswim.dev@gmail.com>
2024-02-15 13:00:04 +0900

    [✨feat]: 기본 아바타 구현

---
+++ src/components/Avatar/Avatar.style.ts
@@ -0,0 +1,65 @@
+import { styled } from 'styled-components';
+
+export type Shape = 'circle' | 'round' | 'square';
+export type Size = 'XS' | 'S' | 'M' | 'L' | 'XL';
+
+const ShapeToStyle: { [key: string]: string } = {
+  circle: '50%',
+  round: '5px',
+  square: '0px',
+};
+
+const SizeToStyle: { [key: string]: string } = {
+  XS: '26px',
+  S: '36px',
+  M: '46px',
+  L: '56px',
+  XL: '66px',
+};
+
+export const AvatarWrapper = styled.div<{
+  size?: Size;
+  shape?: Shape;
+  src?: string;
+  $isBorder?: boolean;
+  $isShadow?: boolean;
+}>`
+  position: relative;
+  box-sizing: border-box;
+  display: inline-block;
+  width: ${({ size }) => size && SizeToStyle[size]};
+  height: ${({ size }) => size && SizeToStyle[size]};
+  overflow: hidden;
+  border: ${({ $isBorder, theme }) =>
+    $isBorder ? `1px solid ${theme.transparent_30}` : 'none'};
+  border-radius: ${({ shape }) => shape && ShapeToStyle[shape]};
+  box-shadow: ${({ $isShadow, theme }) =>
+    $isShadow &&
+    (theme.mode === 'light'
+      ? `0px 4px 8px ${theme.black_primary}20`
+      : `0px 4px 8px #00000050`)};
+  &::before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    content: '';
+    background-color: ${({ theme }) => theme.background_color};
+    background-image: ${({ src, theme }) =>
+      src
+        ? `url(${src})`
+        : theme.mode === 'light'
+          ? 'url(src/assets/avatar-light.png)'
+          : 'url(src/assets/avatar-dark.png)'};
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+`;
+
+export const AvatarContainer = styled.div<{ $isPointer: boolean }>`
+  position: relative;
+  display: inline-block;
+  cursor: ${({ $isPointer }) => $isPointer && 'pointer'};
+`;
---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,48 @@
+import { useMemo } from 'react';
+
+import { AvatarContainer, AvatarWrapper, Shape, Size } from './Avatar.style';
+
+interface AvatarProps {
+  size?: Size;
+  shape?: Shape;
+  src?: string;
+  isBorder?: boolean;
+  isShadow?: boolean;
+  onClick?: React.MouseEventHandler<HTMLElement>;
+}
+
+/**
+ * @param [size='S'] - XS, S, M, L, XL
+ * @param [shape='circle'] - circle은 원형, square는 사각형, round는 둥근 사각형
+ * @param src - 이미지 소스
+ * @param [isBorder=true] - true는 보더(solid) 적용
+ * @param [isShadow=true] - true는 그림자 적용
+ * @param [isEdit=false] - true는 그림자 적용
+ */
+export default function Avatar({
+  size = 'S',
+  shape = 'circle',
+  src,
+  isBorder = true,
+  isShadow = true,
+  onClick,
+  ...props
+}: AvatarProps) {
+  const isPointer = useMemo(() => {
+    return onClick ? true : false;
+  }, []);
+
+  return (
+    <AvatarContainer $isPointer={isPointer}>
+      <AvatarWrapper
+        size={size}
+        shape={shape}
+        src={src}
+        $isBorder={isBorder}
+        $isShadow={isShadow}
+        onClick={onClick}
+        {...props}
+      ></AvatarWrapper>
+    </AvatarContainer>
+  );
+}
