fac804f03609e3203f1802625c4e98a4b19ead0a
suyoung <joyswim.dev@gmail.com>
2024-02-17 01:09:24 +0900

    [✨feat]: fill, shape props에 따른 기능 구현

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,24 +1,31 @@
+import { useMemo } from 'react';
+
+import { Shape } from '@/types';
+
+import { ImageWrapper } from './Image.style';
+
 interface ImageProps {
   src: string;
   alt?: string;
-  width?: number;
-  height?: number;
-  sizes?: string;
-  shape?: string;
-  priority?: boolean;
+  width?: string;
+  height?: string;
+  shape?: Shape;
   fill?: boolean;
+  sizes?: string;
+  priority?: boolean;
   onClick?: (event: React.MouseEvent<HTMLImageElement>) => void;
+  [key: string]: any;
 }
 
 /**
  * @param src - 필수: 이미지 url
- * @param alt - 이미지 대체 텍스트
- * @param width - 가로 너비
- * @param height - 세로 너비
- * @param sizes - 미디어 조건을 달 수 있습니다. 예 "(max-width: 500px) 444px"
+ * @param [alt="이미지"] - 이미지 대체 텍스트
+ * @param width - 가로 너비, 100%의 경우 fill을 사용해주세요.
+ * @param height - 세로 너비, 100%의 경우 fill을 사용해주세요.
  * @param shape - circle, square, round
+ * @param [fill=false] - 부모 요소를 채웁니다. width, height 속성이 없을 때 fill을 사용합니다. 상위 요소는 relative 속성을 가집니다.
+ * @param sizes - 미디어 조건을 달 수 있습니다. 예 "(max-width: 500px) 444px"
  * @param priority - 우선순위 true인 경우 lazy loading과 비동기 디코딩이 적용됩니다.
- * @param [fill=false] - 상위 요소를 채우는 속성. width, height 속성이 없을 때 fill을 사용합니다. 상위 요소는 relative 속성을 가집니다.
  * @param onClick - 마우스 클릭 이벤트
  */
 export default function Image({
@@ -26,24 +33,35 @@ export default function Image({
   alt = '이미지',
   width,
   height,
-  sizes,
-  shape = 'round',
-  priority = false,
+  shape,
   fill = false,
+  sizes,
+  priority = false,
   onClick,
   ...props
 }: ImageProps) {
+  const $isPointer = useMemo(() => {
+    return onClick ? true : false;
+  }, []);
+
   return (
-    <img
-      src={src}
-      alt={alt}
+    <ImageWrapper
       width={width}
       height={height}
-      onClick={onClick}
-      loading={!priority ? 'lazy' : undefined}
-      decoding={!priority ? 'async' : undefined}
-      sizes={sizes}
-      {...props}
-    />
+      shape={shape}
+      $isPointer={$isPointer}
+      $fill={fill}
+    >
+      <img
+        src={src}
+        alt={alt}
+        sizes={sizes}
+        loading={!priority ? 'lazy' : undefined}
+        decoding={!priority ? 'async' : undefined}
+        object-fit={fill ? 'cover' : undefined}
+        onClick={onClick}
+        {...props}
+      />
+    </ImageWrapper>
   );
 }
