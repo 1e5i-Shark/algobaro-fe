4da064668a85fce72611c397e26492e709c16b4a
suyoung <joyswim.dev@gmail.com>
2024-02-19 01:15:16 +0900

    [💬comment]: JSDoc 추가

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -35,6 +35,12 @@ const SpinnerWrapper = styled.div<SpinnerProps>`
   animation: ${spin} 0.7s linear infinite;
 `;
 
+/**
+ * 로딩중에 사용되는 스피너입니다.
+ * @param [size='XS'] - (선택) XS, S, M, L, XL
+ * @param [color='secondary_color'] - (선택) 스피너 색상
+ * @param [weight = '0.3rem'] - (선택) 아이콘 두께
+ */
 export default function Spinner({
   size = 'XS',
   color,
