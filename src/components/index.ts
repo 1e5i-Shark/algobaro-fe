46161ac207cef02b2c30ecc5d3d3eaed2a37c451
suyoung <joyswim.dev@gmail.com>
2024-02-19 01:21:25 +0900

    [💄style]: 내보내기 코드포맷팅

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Avatar, EditButton } from './Avatar';
 export { default as Modal } from './Common/Modal/Modal';
+export { default as Spinner } from './Common/Spinner/Spinner';
 export { default as ThemeContext } from './ThemeContext';
