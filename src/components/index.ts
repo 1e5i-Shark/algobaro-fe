786dbd12858a608a43e1557f9cd1fc4b810004dc
pySoo <soopy368@gmail.com>
2024-02-19 16:02:25 +0900

    [🚚rename]: Common 폴더로 이동

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { Avatar, EditButton } from './Avatar';
-export { default as Button } from './Button/Button';
+export { default as Button } from './Common/Button/Button';
 export { default as Modal } from './Common/Modal/Modal';
 export { default as ThemeContext } from './ThemeContext';
