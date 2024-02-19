c30febe12d977b70c1a1f0abfb329a401fe2d2f5
Suyoung Shin <joyswim.dev@gmail.com>
2024-02-19 18:06:47 +0900

    [✨feat]: ThemeCustomProvider Context 추가

---
+++ src/components/ThemeCustomProvider.tsx
@@ -0,0 +1,60 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import { useLocalStorage } from '@/hooks/useLocalStorage';
+import { darkTheme, lightTheme, ThemeType } from '@/styles/theme';
+
+type ThemeModeType = 'light' | 'dark';
+
+export interface ThemeContextProps {
+  theme: ThemeType;
+  toggleTheme: () => void;
+}
+
+interface ThemeProps {
+  children: ReactNode;
+}
+
+export const ThemeCustomContext = createContext({
+  theme: lightTheme,
+  toggleTheme: () => {},
+});
+
+export function ThemeCustomProvider({ children }: ThemeProps) {
+  const [themeMode, setThemeMode] = useState<ThemeModeType | null>(null);
+
+  const [localTheme, setLocalTheme] = useLocalStorage('theme');
+
+  const getSystemTheme = () => {
+    const systemTheme = window.matchMedia(
+      '(prefers-color-scheme: light)'
+    ).matches;
+    return systemTheme ? 'light' : 'dark';
+  };
+
+  const toggleTheme = () => {
+    const toggledTheme = themeMode === 'light' ? 'dark' : 'light';
+    setThemeMode(toggledTheme);
+    setLocalTheme(toggledTheme);
+  };
+
+  useEffect(() => {
+    setThemeMode(getSystemTheme());
+  }, []);
+
+  useEffect(() => {
+    if (!localTheme) {
+      const theme = getSystemTheme();
+
+      setThemeMode(theme);
+    }
+  }, [localTheme]);
+
+  const currentTheme = themeMode === 'light' ? lightTheme : darkTheme;
+
+  return (
+    <ThemeCustomContext.Provider value={{ theme: currentTheme, toggleTheme }}>
+      <ThemeProvider theme={currentTheme}>{children}</ThemeProvider>;
+    </ThemeCustomContext.Provider>
+  );
+}
