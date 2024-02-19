a12558e5b4c037d70422a76b60f4ac09b43fab80
Cho-Ik-Jun <harry960629@naver.com>
2024-02-19 19:34:04 +0900

    [💬comment]: JSDoc 및 함수 관련 주석 추가

--- src/components/Common/Tag/Tag.tsx
+++ src/components/Common/Tag/Tag.tsx
@@ -17,6 +17,19 @@ interface TagProps extends HTMLAttributes<HTMLDivElement> {
   onDeleted?: (tagName: string) => void;
 }
 
+/**
+ * Tag 컴포넌트
+ * @param [mode = 'normal'] - 필수, `normal`(기본값), `select`, `delete` 모드 설정 가능
+ * @param [width = 'fit-content'] - 태그 너비
+ * @param [height = '3rem'] - 태그 높이
+ * @param [width = 'fit-content'] - 태그 너비
+ * @param [fontSize = '1.6rem'] - 태그 폰트 사이즈
+ * @param [backgroundColor = 'gray_50'] - 태그 배경, 선택 태그의 경우 전역 기본 배경색
+ * @param [borderColor = {기본 배경색}] - border 색
+ * @param [onSelected] - 선택 시 태그 이름 외부로 전달
+ * @param [onDeleted] - 삭제 시 태그 이름 외부로 전달
+ * @returns
+ */
 export default function Tag({
   mode = 'normal',
   width = 'fit-content',
@@ -34,11 +47,13 @@ export default function Tag({
 
   const tagName = children?.toString();
 
+  // 태그 선택 handler 함수
   const handleSelectTag = () => {
     setIsTagSelect(!isTagSelect);
     tagName && onSelected && onSelected(tagName);
   };
 
+  // 태그 삭제 handler 함수
   const handleDeleteTag = () => {
     tagName && onDeleted && onDeleted(tagName);
   };
