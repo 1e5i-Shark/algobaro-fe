2ea5d9807b7e381c3d2984fac6392a547e427484
Suyoung Shin <joyswim.dev@gmail.com>
2024-02-19 18:41:30 +0900

    [♻️refactor]: 디버깅 코드 추가

--- src/hooks/useCustomTheme.ts
+++ src/hooks/useCustomTheme.ts
@@ -6,8 +6,13 @@ import {
 } from '@/components/ThemeCustomProvider';
 
 export const useCustomTheme = () => {
-  const { theme, toggleTheme }: ThemeContextProps =
-    useContext(ThemeCustomContext);
+  const themeContext: ThemeContextProps = useContext(ThemeCustomContext);
+
+  if (!themeContext) {
+    throw new Error('Error: ThemeContext가 존재하지 않습니다.');
+  }
+
+  const { theme, toggleTheme } = themeContext;
 
   return { theme, toggleTheme };
 };
