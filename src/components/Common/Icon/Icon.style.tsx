64730033fc5a083c21c4876a6e6dd33eb85ac0f5
타래 <85001878+meowTarae@users.noreply.github.com>
2024-02-21 14:41:44 +0900

    [🎨design]: CSS 속성 수정
    - 기본 props 값 설정, 그림자, hover 시 밝기 조절

--- src/components/Common/Icon/Icon.style.tsx
+++ src/components/Common/Icon/Icon.style.tsx
@@ -13,11 +13,11 @@ export const IconWrapper = styled.button<IconWrapperProps>`
     width: ${$background && $backgroundSize};
     height: ${$background && $backgroundSize};
     background-color: ${$background && theme.color.transparent_10};
-    border-radius: ${$background ? '50%' : '0'};
-    box-shadow: ${$background && '0 12px 20px -12px rgba(0, 0, 0, .35)'};
+    border-radius: ${$background && '50%'};
+    box-shadow: ${$background && '0px 2px 3px 0px rgba(10, 10, 10, .3)'};
 
     &:hover {
-      filter: brightness(90%);
+      filter: brightness(50%);
     }
   `}
 `;
--- src/components/Common/Icon/Icon.tsx
+++ src/components/Common/Icon/Icon.tsx
@@ -1,12 +1,15 @@
 import { cloneElement, HTMLAttributes, MouseEvent, ReactElement } from 'react';
 
+import { useCustomTheme } from '@/hooks/useCustomTheme';
+
 import { IconWrapper } from './Icon.style';
 
 /**
  * @param [children] - 아이콘을 받습니다. 아이콘 이름을 컨벤션에 맞게끔 맨 뒤에 '-Rounded' 붙여주세요!
  * @param [color] - 아이콘 색상을 지정해줄 수 있습니다.
  * @param [size] - 아이콘의 크기를 지정해줄 수 있습니다.
- * @param [background] - 배경 여부를 결정합니다.
+ * @param [mode] - 테마를 설정할 수 있습니다. (ex - 라이트/다크모드)
+ * @param [background = false] - 배경 여부를 결정합니다.
  * @param [backgroundSize] - 배경의 크기를 지정해줄 수 있습니다.
  * @param [onClick] - 마우스 클릭 핸들러 함수를 props로 받습니다.
  */
@@ -22,15 +25,20 @@ interface IconProps extends HTMLAttributes<HTMLButtonElement> {
 
 export default function Icon({
   children,
-  color = '#909090',
-  size = '1.5rem',
+  color = '',
+  size = '',
   background = false,
-  backgroundSize = '2rem',
+  backgroundSize = '3.6rem',
   onClick,
   ...props
 }: IconProps) {
+  const { theme } = useCustomTheme();
+
   const cloneIcon = cloneElement(children, {
-    sx: { color, size },
+    sx: {
+      color: color || theme.color.gray_50,
+      fontSize: size || theme.size.icon.XS,
+    },
   });
 
   return (
