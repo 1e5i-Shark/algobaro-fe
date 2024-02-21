b7df3c533e97cad1876f12cc684a80357f944b88
타래 <85001878+meowTarae@users.noreply.github.com>
2024-02-21 14:37:26 +0900

    [♻️refactor]: 불필요한 props인 mode 제거

--- src/components/Common/Icon/Icon.style.tsx
+++ src/components/Common/Icon/Icon.style.tsx
@@ -1,25 +1,20 @@
 import styled, { css } from 'styled-components';
 
 interface IconWrapperProps {
-  $mode: string;
   $background: boolean;
   $backgroundSize: string;
 }
 
 export const IconWrapper = styled.button<IconWrapperProps>`
-  ${({ $mode, $background, $backgroundSize, theme }) => css`
+  ${({ $background, $backgroundSize, theme }) => css`
     display: flex;
     align-items: center;
     justify-content: center;
     width: ${$background && $backgroundSize};
     height: ${$background && $backgroundSize};
-    background-color: ${$background
-      ? $mode === 'light'
-        ? theme.color.white_primary
-        : theme.color.transparent_10
-      : 'none'};
+    background-color: ${$background && theme.color.transparent_10};
     border-radius: ${$background ? '50%' : '0'};
-    box-shadow: ${$mode && '0 12px 20px -12px rgba(0, 0, 0, .35)'};
+    box-shadow: ${$background && '0 12px 20px -12px rgba(0, 0, 0, .35)'};
 
     &:hover {
       filter: brightness(90%);
--- src/components/Common/Icon/Icon.tsx
+++ src/components/Common/Icon/Icon.tsx
@@ -6,7 +6,6 @@ import { IconWrapper } from './Icon.style';
  * @param [children] - 아이콘을 받습니다. 아이콘 이름을 컨벤션에 맞게끔 맨 뒤에 '-Rounded' 붙여주세요!
  * @param [color] - 아이콘 색상을 지정해줄 수 있습니다.
  * @param [size] - 아이콘의 크기를 지정해줄 수 있습니다.
- * @param [mode] - 테마를 설정할 수 있습니다. (ex - 라이트/다크모드)
  * @param [background] - 배경 여부를 결정합니다.
  * @param [backgroundSize] - 배경의 크기를 지정해줄 수 있습니다.
  * @param [onClick] - 마우스 클릭 핸들러 함수를 props로 받습니다.
@@ -16,7 +15,6 @@ interface IconProps extends HTMLAttributes<HTMLButtonElement> {
   children: ReactElement;
   color?: string;
   size?: string;
-  mode?: '' | 'light' | 'dark';
   background?: boolean;
   backgroundSize?: string;
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
@@ -26,7 +24,6 @@ export default function Icon({
   children,
   color = '#909090',
   size = '1.5rem',
-  mode = '',
   background = false,
   backgroundSize = '2rem',
   onClick,
@@ -39,7 +36,6 @@ export default function Icon({
   return (
     <IconWrapper
       onClick={onClick}
-      $mode={mode}
       $background={background}
       $backgroundSize={backgroundSize}
       {...props}
