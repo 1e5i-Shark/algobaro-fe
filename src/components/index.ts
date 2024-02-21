c971911bdf5b186797216dabcf5050b5e4ec677a
타래 <85001878+meowTarae@users.noreply.github.com>
2024-02-21 14:58:51 +0900

    [✨feat]: Components index 파일에 Menu 경로 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Avatar, EditButton } from './Common/Avatar';
+export { default as Menu } from './Common/Menu/Menu';
 export { default as Modal } from './Common/Modal/Modal';
 export { ThemeCustomProvider } from './ThemeCustomProvider';
