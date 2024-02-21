70e53e7fc6db7cd56228285e54843d285ce4f6b2
Suyoung Shin <joyswim.dev@gmail.com>
2024-02-21 15:53:37 +0900

    [🚚rename]: Image 컴포넌트 Common 폴더로 이동

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export { Avatar, EditButton } from './Common/Avatar';
+export { default as Image } from './Common/Image/Image';
 export { default as Modal } from './Common/Modal/Modal';
 export { default as Spinner } from './Common/Spinner/Spinner';
 export { default as Tag } from './Common/Tag/Tag';
-export { default as Image } from './Image/Image';
 export { ThemeCustomProvider } from './ThemeCustomProvider';
