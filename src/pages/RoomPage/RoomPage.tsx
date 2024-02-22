4d9aadd5e679231741db1d60802a9f1bf0dd2461
Suyoung Shin <joyswim.dev@gmail.com>
2024-02-22 11:45:32 +0900

    [🎨design]: 초기 레이아웃 작업

--- src/pages/RoomPage/RoomPage.tsx
+++ src/pages/RoomPage/RoomPage.tsx
@@ -1,3 +1,33 @@
+import { styled } from 'styled-components';
+
+import { Col, Row } from '@/styles/GlobalStyle';
+
+const RoomContainer = styled(Row)`
+  height: 100vh;
+  background-color: 'peru';
+`;
+
+const WaitingRoomContainer = styled(Col)`
+  flex-grow: 3;
+  min-width: 50rem;
+  background-color: #789456;
+`;
+
+const ChatContainer = styled(Col)`
+  flex-grow: 1;
+  min-width: 25rem;
+  background-color: #f2f13f;
+`;
+
 export default function RoomPage() {
-  return <div>RoomPage</div>;
+  return (
+    <RoomContainer>
+      <WaitingRoomContainer>
+        <h1>대기룸</h1>
+      </WaitingRoomContainer>
+      <ChatContainer>
+        <h1>채팅</h1>
+      </ChatContainer>
+    </RoomContainer>
+  );
 }
