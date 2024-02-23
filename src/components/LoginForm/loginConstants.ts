3419d6fa189669d668e10ae88d5cecbd2c381dbf
Cho-Ik-Jun <harry960629@naver.com>
2024-02-24 04:21:14 +0900

    [♻️refactor]: 이메일 유효성 에러 문구 변경

--- src/components/LoginForm/loginConstants.ts
+++ src/components/LoginForm/loginConstants.ts
@@ -2,7 +2,7 @@ export const LOGIN_VALIDATION = {
   EMAIL: {
     pattern: {
       value: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-z]+[.]+[a-z]{2,3}$/,
-      message: '이메일 형식과 맞지 않습니다.',
+      message: '이메일 형식이 아닙니다.',
     },
   },
 };
