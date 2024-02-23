ff8d02b49b892d774442571bcefe5704546af99e
Cho-Ik-Jun <harry960629@naver.com>
2024-02-23 19:57:52 +0900

    [🐛fix]: 이메일 유효성 검사 정규식 수정

--- src/components/LoginForm/loginConstants.ts
+++ src/components/LoginForm/loginConstants.ts
@@ -1,7 +1,7 @@
 export const LOGIN_VALIDATION = {
   EMAIL: {
     pattern: {
-      value: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-z]*.[a-z]{2,3}$/,
+      value: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-z]+[.]+[a-z]{2,3}$/,
       message: '이메일 형식과 맞지 않습니다.',
     },
   },
