94d413400db61e6f57afef36d555f75b1ed09929
suyoung <joyswim.dev@gmail.com>
2024-02-25 22:33:12 +0900

    [✨feat]: RoomHeaderButtons 컴포넌트 생성 초기 레이아웃 잡기

--- src/pages/RoomPage/RoomPage.style.ts
+++ src/pages/RoomPage/RoomPage.style.ts
@@ -25,3 +25,62 @@ export const ChatContainer = styled(Col)`
     border-left: 1px solid ${theme.color.transparent_30};
   `}
 `;
+
+// RoomHeader
+export const HeaderContainer = styled(Row)`
+  justify-content: space-between;
+  width: 100%;
+  /* background-color: lightblue; */
+`;
+
+export const RoomInfoContainer = styled.div`
+  background-color: 'peru';
+`;
+
+export const ButtonsWrapper = styled(Row)``;
+
+export const RoomIdWrapper = styled(Row)`
+  padding-bottom: 0.8rem;
+
+  &:hover {
+    filter: brightness(95%);
+  }
+`;
+
+export const CopyRoomIdTag = styled(Row)`
+  ${({ theme }) => css`
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+    background-color: ${theme.color.container_color};
+    border-radius: 0.3rem;
+  `}
+`;
+
+export const TextId = styled.div`
+  padding-right: 0.3rem;
+`;
+
+export const TitleWrapper = styled(Row)`
+  align-items: center;
+  padding-bottom: 0.7rem;
+`;
+
+export const TagsWrapper = styled(Row)`
+  padding-bottom: 0.7rem;
+`;
+
+export const TagWrapper = styled.div`
+  padding-right: 0.5rem;
+`;
+
+export const LogosWrapper = styled(Row)``;
+
+export const LogoWrapper = styled.div`
+  width: 2.5rem;
+  height: 2.5rem;
+  margin-right: 0.5rem;
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+`;
--- src/pages/RoomPage/RoomViews/Header/RoomHeaderButtons.tsx
+++ src/pages/RoomPage/RoomViews/Header/RoomHeaderButtons.tsx
@@ -1,11 +1,60 @@
+import {
+  DarkModeRounded,
+  ExitToAppRounded,
+  LightModeRounded,
+} from '@mui/icons-material';
+
+import { Button, Icon } from '@/components';
+import { useCustomTheme } from '@/hooks/useCustomTheme';
+import { ButtonsWrapper } from '@/pages/RoomPage/RoomPage.style';
+
 interface RoomButtonsProps {
   className: string;
 }
 
 export default function RoomHeaderButtons({ className }: RoomButtonsProps) {
+  const { theme, toggleTheme } = useCustomTheme();
   return (
-    <div className={className}>
-      <h1>RoomHeaderButtons</h1>
-    </div>
+    <ButtonsWrapper className={className}>
+      <Button
+        className="changeRoomInfo"
+        fontSize="1.4rem"
+        onClick={() => {
+          alert('changeRoomInfo');
+        }}
+        style={{ marginRight: '1rem' }}
+      >
+        방 정보 변경
+      </Button>
+      {theme.mode === 'dark' ? (
+        <Icon
+          className="darkTheme"
+          background={true}
+          onClick={() => {
+            toggleTheme();
+          }}
+          style={{ marginRight: '1rem' }}
+        >
+          <DarkModeRounded fontSize="large" />
+        </Icon>
+      ) : (
+        <Icon
+          className="lightTheme"
+          background={true}
+          onClick={() => toggleTheme()}
+          style={{ marginRight: '1rem' }}
+        >
+          <LightModeRounded fontSize="large" />
+        </Icon>
+      )}
+      <Icon
+        className="exitRoom"
+        background={true}
+        onClick={() => alert('RoomExit')}
+        style={{ marginRight: '1rem' }}
+      >
+        <ExitToAppRounded fontSize="large" />
+      </Icon>
+    </ButtonsWrapper>
   );
 }
