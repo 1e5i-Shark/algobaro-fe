dc91d74f6435b42f7a5a00ad8c237da63c4cb747
suyoung <joyswim.dev@gmail.com>
2024-02-25 22:08:43 +0900

    [✨feat]: 프로그래밍 언어 로고 상수 선언

---
+++ src/constants/logos.ts
@@ -0,0 +1,10 @@
+export const LOGOS: { [key: string]: string } = {
+  python:
+    'https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1566791609/noticon/nen1y11gazeqhejw7nm1.png',
+  java: 'https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1566913897/noticon/xbvewg1m3azbpnrzck1k.png',
+  javascript:
+    'https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1566557375/noticon/reezsa2psnh8moufkd5l.png',
+  javascript2:
+    'https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1567008394/noticon/ohybolu4ensol1gzqas1.png',
+  cpp: 'https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1640252210/noticon/ux6lhnhzbskc4t8xf0tl.png',
+};
