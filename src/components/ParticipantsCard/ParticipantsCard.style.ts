2f0f3fcb39ce71567fc3cbea5d8472bf0c34052c
Suyoung Shin <joyswim.dev@gmail.com>
2024-02-26 22:06:16 +0900

    [✨feat]: 참가자 정보를 나타내는 ParticipantsCard 컴포넌트 추가

---
+++ src/components/ParticipantsCard/ParticipantsCard.style.ts
@@ -0,0 +1,45 @@
+import { css, styled } from 'styled-components';
+
+import { Col, Row } from '@/styles/GlobalStyle';
+
+export const CardWrapper = styled(Col)`
+  ${({ theme }) => css`
+    position: relative;
+    flex-shrink: 0;
+    gap: 1rem;
+    align-items: center;
+    width: ${theme.WIDTH.CARD};
+    height: 22rem;
+    padding-top: 4.5rem;
+    margin-right: 2rem;
+    border-radius: 1rem;
+    box-shadow: 0 4px 8px 0 ${theme.color.transparent_30};
+  `}
+`;
+
+export const MenuWrapper = styled.div`
+  position: absolute;
+  top: 2rem;
+  right: 2rem;
+`;
+
+export const NameWrapper = styled.div``;
+
+export const StatusWrapper = styled(Row)`
+  ${({ theme }) => css`
+    position: absolute;
+    bottom: 2rem;
+    justify-content: center;
+    padding-top: 2rem;
+    border-top: 1px solid ${theme.color.transparent_50};
+  `}
+`;
+
+export const StatusText = styled.span<{ $color: string }>`
+  ${({ $color, theme }) => css`
+    padding-right: 0.3rem;
+    font-size: ${theme.size.L};
+    font-weight: ${theme.fontWeight.semiBold};
+    color: ${$color};
+  `}
+`;
---
+++ src/components/ParticipantsCard/ParticipantsCard.tsx
@@ -0,0 +1,52 @@
+import MoreVertRoundedIcon from '@mui/icons-material/MoreVertRounded';
+
+import { Avatar } from '@/components/Common/Avatar';
+
+import Icon from '../Common/Icon/Icon';
+import Menu from '../Common/Menu/Menu';
+import { MenuText } from '../Common/Menu/MenuText';
+import * as S from './ParticipantsCard.style';
+
+interface ParticipantsProps {
+  username: string;
+  image: string;
+  status: string;
+}
+
+export default function ParticipantsCard({
+  username,
+  image,
+}: ParticipantsProps) {
+  const menuList = [
+    {
+      id: 1,
+      text: MenuText.TransferHost,
+      onClick: () => alert('방장 위임 클릭!'),
+    },
+    {
+      id: 2,
+      text: MenuText.KickOut,
+      onClick: () => alert('강제 퇴장 클릭!'),
+    },
+  ];
+
+  return (
+    <S.CardWrapper>
+      <S.MenuWrapper>
+        <Menu
+          menuList={menuList}
+          className="menu"
+        >
+          <Icon>
+            <MoreVertRoundedIcon />
+          </Icon>
+        </Menu>
+      </S.MenuWrapper>
+      <Avatar
+        src={image}
+        size="M"
+      />
+      <S.NameWrapper>{username}</S.NameWrapper>
+    </S.CardWrapper>
+  );
+}
