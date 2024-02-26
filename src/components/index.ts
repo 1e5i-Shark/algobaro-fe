d19f610106665aeff179ba040cff5e86b649af4f
pySoo <soopy368@gmail.com>
2024-02-27 00:05:35 +0900

    [✨feat]: Common 컴포넌트 index.ts에 모두 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,12 @@
 export { Avatar, EditButton } from './Common/Avatar';
 export { default as Button } from './Common/Button/Button';
+export { default as CheckBox } from './Common/CheckBox/CheckBox';
+export { DropDown, MultiDropDown } from './Common/DropDown';
+export { default as EllipsisText } from './Common/EllipsisText/EllipsisText';
 export { default as Header } from './Common/Header/Header';
 export { default as Icon } from './Common/Icon/Icon';
 export { default as Image } from './Common/Image/Image';
+export { default as Input } from './Common/Input/Input';
 export { default as Menu } from './Common/Menu/Menu';
 export { default as Message } from './Common/Message/Message';
 export { default as Modal } from './Common/Modal/Modal';
