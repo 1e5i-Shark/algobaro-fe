d6013be627462cf68affa65eca4a7e06cebfe395
Cho-Ik-Jun <harry960629@naver.com>
2024-02-27 18:01:03 +0900

    [✨feat]: axios patch 타입 추가

--- src/types/api.ts
+++ src/types/api.ts
@@ -4,6 +4,11 @@ export interface CustomInstance extends AxiosInstance {
   get<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
   post<T>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T>;
   delete<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+  patch<T>(
+    url: string,
+    data?: unknown,
+    config?: AxiosRequestConfig
+  ): Promise<T>;
 }
 
 export type CustomResponseType<T = unknown> = {
