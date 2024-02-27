da55f5e9faebfeb3a1b653502f91ab7bbf7626e8
Suyoung Shin <joyswim.dev@gmail.com>
2024-02-27 22:56:11 +0900

    [♻️refactor]: 룸 관련 API 타입 추가

--- src/types/room.ts
+++ src/types/room.ts
@@ -1,4 +1,7 @@
-export interface UpdateRoomType {
+// 개별 방 정보 조회
+export interface RoomType {
+  roomId: number;
+  roomStatus: string;
   title: string;
   introduce: string;
   roomAccessType: string;
@@ -6,18 +9,52 @@ export interface UpdateRoomType {
   problemName: string;
   password: string;
   roomLimit: number;
-  levelTag: string;
-  algorithmTag: string;
-
+  tags: string;
+  timeLimit: number;
+  roomUUID: string;
+  // Todo: 백엔드 요청 보류
   shortUUID: string;
-  problemLink: string;
-  tags: string[];
   languages: string[];
-  users: UserType[];
-  timer: number;
+  users: User[];
+  problemLink: string;
 }
 
-export interface UserType {
+// 방 수정 API
+export interface UpdateRoom {
+  title: string;
+  introduce: string;
+  startAt: string;
+  roomAccessType: string;
+  problemLink: string;
+  problemPlatform: string;
+  problemName: string;
+  password: string;
+  roomLimit: number;
+  tags: string;
+  timeLimit: number;
+}
+
+export interface RoomResponse {
+  roomId: number;
+  roomStatus: string;
+  title: string;
+  introduce: string;
+  roomAccessType: string;
+  problemPlatform: string;
+  problemName: string;
+  password: string;
+  roomLimit: number;
+  tags: string;
+  timeLimit: number;
+  roomUUID: string;
+}
+
+export interface ChangeHostResponse {
+  id: number;
+  roomId: number;
+}
+
+export interface User {
   username: string;
   status: string;
   image: string;
