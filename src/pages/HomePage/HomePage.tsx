25f410faafe1c6952ecf4a8e2284a1c86608cb6b
타래 <85001878+meowTarae@users.noreply.github.com>
2024-02-28 01:16:02 +0900

    [✨feat]: HomePage Nav 구현

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,49 @@
+import RefreshRoundedIcon from '@mui/icons-material/RefreshRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+
+import { Button, CheckBox, Icon } from '@/components';
+
+import * as S from './HomePage.style';
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const latestUpdate = '1분전';
+
+  return (
+    <S.HomeContainer>
+      <S.HomeWrapper>
+        <S.NavContainer>
+          <S.NavWrapper>
+            <Button>방 만들기</Button>
+            <S.HomeNavWrapper>
+              <S.SearchWrapper>
+                <S.SearchInput
+                  type="text"
+                  placeholder="방 제목을 검색해 주세요."
+                />
+                <Icon>
+                  <SearchRoundedIcon />
+                </Icon>
+              </S.SearchWrapper>
+              <div>dropdown</div>
+              <CheckBox
+                label="비밀방"
+                checked={true}
+              />
+              <CheckBox
+                label="입장 가능"
+                checked={true}
+              />
+            </S.HomeNavWrapper>
+          </S.NavWrapper>
+          <S.UpdateData>
+            {/* 글자 크기 작게 수정하기 */}
+            {`마지막 업데이트: ${latestUpdate}`}
+            <Icon>
+              <RefreshRoundedIcon />
+            </Icon>
+          </S.UpdateData>
+        </S.NavContainer>
+      </S.HomeWrapper>
+    </S.HomeContainer>
+  );
 }
