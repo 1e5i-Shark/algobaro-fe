906657cdf446321a62064180d68e942b80de537e
타래 <85001878+meowTarae@users.noreply.github.com>
2024-02-28 01:20:15 +0900

    [✨feat]: HomePage Main Section 구현

--- src/pages/HomePage/HomePage.style.ts
+++ src/pages/HomePage/HomePage.style.ts
@@ -73,13 +73,87 @@ const UpdateData = styled.span`
   }
 `;
 
+// ####################### Section #######################
+
+const HomeMain = styled.main`
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 2rem;
+  height: 58rem;
+`;
+
+const MainSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 3rem;
+  background-color: ${({ theme }) => theme.color.gray_10};
+  border-radius: 12px;
+  box-shadow: 0px 2px 3px 1px rgba(200, 200, 200, 3);
+`;
+
+const SectionUpperArea = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+  font-size: ${({ theme }) => theme.size.L};
+`;
+
+const UpperAreaWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  & :first-child {
+    margin-right: 0.5rem;
+  }
+`;
+
+const SectionTitle = styled.span``;
+
+const SectionLimit = styled.span``;
+
+const SectionBelowArea = styled.div`
+  display: flex;
+  overflow-x: auto;
+
+  & > * {
+    flex-shrink: 0;
+    margin-right: 0.5rem;
+  }
+`;
+
+const SectionLowerArea = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const LowerAreaImageWrapper = styled.div`
+  display: flex;
+
+  & > * {
+    margin-right: 2rem;
+  }
+`;
+
 export {
   HomeContainer,
+  HomeMain,
   HomeNavWrapper,
   HomeWrapper,
+  LowerAreaImageWrapper,
+  MainSection,
   NavContainer,
   NavWrapper,
   SearchInput,
   SearchWrapper,
+  SectionBelowArea,
+  SectionLimit,
+  SectionLowerArea,
+  SectionTitle,
+  SectionUpperArea,
   UpdateData,
+  UpperAreaWrapper,
 };
