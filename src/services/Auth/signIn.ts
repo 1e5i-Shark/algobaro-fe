680e0682bf1a026163cef730e087fb5705268ab1
Cho-Ik-Jun <harry960629@naver.com>
2024-02-27 17:40:02 +0900

    [✨feat]: signIn 함수 반환값 타입 추가

--- src/services/Auth/signIn.ts
+++ src/services/Auth/signIn.ts
@@ -2,19 +2,25 @@ import { SIGNIN_URL } from '@/components/LoginForm/loginConstants';
 
 import { axiosInstance } from '../axiosInstance';
 
+interface signInResponse {
+  success: boolean;
+  response: {
+    accessToken: string;
+  };
+}
+
 const signIn = async (email: string, password: string) => {
   const reqBody = {
     email,
     password,
   };
 
-  try {
-    const res = await axiosInstance.post(`${SIGNIN_URL}`, reqBody);
+  const res: signInResponse = await axiosInstance.post(
+    `${SIGNIN_URL}`,
+    reqBody
+  );
 
-    console.log('응답:', res);
-  } catch (e) {
-    console.error(e);
-  }
+  return res;
 };
 
 export default signIn;
