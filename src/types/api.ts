8e751dcd0f5524282eac89ec779c8bf2f64d9a10
Cho-Ik-Jun <harry960629@naver.com>
2024-02-27 17:22:07 +0900

    [✨feat]: axios 관련 타입 추가

---
+++ src/types/api.ts
@@ -0,0 +1,20 @@
+import { AxiosInstance, AxiosRequestConfig } from 'axios';
+
+export interface CustomInstance extends AxiosInstance {
+  get<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+  post<T>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T>;
+  delete<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+}
+
+export type CustomResponseType<T = unknown> = {
+  status: number;
+  data?: T;
+};
+
+export interface errorData {
+  success: boolean;
+  error: {
+    errorCode: string;
+    message: string;
+  };
+}
