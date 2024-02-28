117d9f737fbaf592036f3e857b48e6acf38d71d8
pySoo <soopy368@gmail.com>
2024-02-28 15:56:58 +0900

    [✨feat]: * 기호가 붙은 타이틀 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -14,4 +14,5 @@ export { default as Modal } from './Common/Modal/Modal';
 export { default as Spinner } from './Common/Spinner/Spinner';
 export { default as Tag } from './Common/Tag/Tag';
 export { default as ThemeModeToggleButton } from './Common/ThemeModeToggleButton/ThemeModeToggleButton';
+export { default as TitleWithAsterisk } from './CreateRoom/TitleWithAsterisk/TitleWithAsterisk';
 export { ThemeCustomProvider } from './ThemeCustomProvider';
