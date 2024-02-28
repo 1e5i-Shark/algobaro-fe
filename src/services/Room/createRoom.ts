511b1e586dc1247e4ad51c4e1e7875de28aef1c6
pySoo <soopy368@gmail.com>
2024-02-28 23:44:13 +0900

    [✨feat]: createRoom API 함수, react-query hook 추가

---
+++ src/services/Room/createRoom.ts
@@ -0,0 +1,27 @@
+import { ROOMS_URL } from '../apiEndpoint';
+import { axiosAuthInstance } from '../axiosInstance';
+
+export interface CreateRoomRequest {
+  roomStatus: string; // [RECRUTING, RUNNING]
+  title: string;
+  roomAccessType: string; // [PRIVATE, PUBLIC]
+  roomLimit: number;
+  startAt: string; // ($date-time) 2024-02-27T13:36:49.089Z
+  password?: string;
+  tags?: string[];
+  timeLimit?: number;
+}
+
+export interface CreateRoomResponse {
+  roomId: number;
+  roomStatus: string; // [RECRUTING, RUNNING]
+  title: string;
+  roomAccessType: string; // [PRIVATE, PUBLIC]
+  roomLimit: number;
+  tags?: string[];
+  timeLimit?: number;
+}
+
+export const createRoom = (request: CreateRoomRequest) => {
+  return axiosAuthInstance.post<CreateRoomResponse>(ROOMS_URL, request);
+};
