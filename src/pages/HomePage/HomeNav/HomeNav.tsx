ad3c770c8ec5d939070859fcc4cc8d760877c4c0
타래 <85001878+meowTarae@users.noreply.github.com>
2024-03-01 01:06:48 +0900

    [💄style]: dropdown 디자인 수정

--- src/pages/HomePage/HomeNav/HomeNav.tsx
+++ src/pages/HomePage/HomeNav/HomeNav.tsx
@@ -1,11 +1,15 @@
 import RefreshRoundedIcon from '@mui/icons-material/RefreshRounded';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import { useState } from 'react';
 
 import { Button, CheckBox, Icon, MultiDropDown } from '@/components';
+import { useCustomTheme } from '@/hooks/useCustomTheme';
 
 import * as S from './HomeNav.style';
 
 export default function HomeNav() {
+  const { theme } = useCustomTheme();
+  const [check, setCheck] = useState(false);
   const dataSet: { [key: string]: string } = {
     python: '파이썬',
     javascript: '자바스크립트',
@@ -34,12 +38,16 @@ export default function HomeNav() {
           dataSet={dataSet}
           labelId="search-code-language-label"
           labelName="언어"
+          fontSize={theme.size.M}
           width="13.3rem"
         />
+
         <CheckBox
           label="비밀방"
-          checked={true}
+          checked={check}
+          onChange={() => setCheck(!check)}
         />
+
         <CheckBox
           label="입장 가능"
           checked={true}
