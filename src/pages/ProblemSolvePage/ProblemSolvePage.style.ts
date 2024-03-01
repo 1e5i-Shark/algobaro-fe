79939ac8865fc096907b043776d313a3c4dc1ca3
pySoo <soopy368@gmail.com>
2024-03-02 00:01:01 +0900

    [🎨design]: ProblemSolvePage 스타일링 적용

---
+++ src/pages/ProblemSolvePage/ProblemSolvePage.style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+import { Col } from '@/styles/GlobalStyle';
+
+const Wrapper = styled(Col)``;
+
+const ModalWrapper = styled(Col)`
+  gap: 2rem;
+  align-items: center;
+  padding: 3rem;
+`;
+
+const ModalTitle = styled.span``;
+
+export { ModalTitle, ModalWrapper, Wrapper };
