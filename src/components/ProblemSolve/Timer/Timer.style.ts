0b88d6fd782ac3efe4c894a0457774962feeaa8d
pySoo <soopy368@gmail.com>
2024-03-02 00:00:32 +0900

    [🎨design]: font 색상을 검정색으로 변경

--- src/components/ProblemSolve/Timer/Timer.style.ts
+++ src/components/ProblemSolve/Timer/Timer.style.ts
@@ -6,6 +6,7 @@ const Wrapper = styled(Row)`
   ${({ theme }) => css`
     width: fit-content;
     padding: 0.7rem 2.5rem;
+    color: ${theme.color.black_primary};
     background-color: ${theme.color.gray_20};
     border-radius: 3rem;
   `}
