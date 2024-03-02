e9890860c31be4861e7b2eecb2751e0ac2a8b3e5
pySoo <soopy368@gmail.com>
2024-03-02 17:47:35 +0900

    [✨feat]: PSHeader 포기하기 버튼에 confirm 기능 추가

--- src/components/Common/Header/PSHeader.tsx
+++ src/components/Common/Header/PSHeader.tsx
@@ -1,3 +1,36 @@
+import { useNavigate } from 'react-router-dom';
+
+import { useCustomTheme } from '@/hooks/useCustomTheme';
+import { PATH } from '@/routes/path';
+
+import Button from '../Button/Button';
+import ThemeModeToggleButton from '../ThemeModeToggleButton/ThemeModeToggleButton';
+import * as S from './PSHeader.style';
+
 export default function PSHeader() {
-  return <header>이건 Component의 PSHeader</header>;
+  const { theme } = useCustomTheme();
+
+  const navigate = useNavigate();
+
+  const handleGiveUp = () => {
+    if (confirm('정말 포기하고 나가시겠습니까?')) {
+      navigate(PATH.HOME);
+    }
+  };
+
+  return (
+    <S.HeaderWrapper>
+      <S.TimerWrapper>타이머</S.TimerWrapper>
+      <S.ButtonWrapper>
+        <ThemeModeToggleButton />
+        <Button
+          height="4rem"
+          backgroundColor={theme.color.red}
+          onClick={handleGiveUp}
+        >
+          포기하기
+        </Button>
+      </S.ButtonWrapper>
+    </S.HeaderWrapper>
+  );
 }
