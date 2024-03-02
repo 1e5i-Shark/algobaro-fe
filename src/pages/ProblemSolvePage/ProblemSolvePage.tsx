f3972a256fff8e1e0808dbf09cd3b2b1884b6a18
pySoo <soopy368@gmail.com>
2024-03-02 17:52:06 +0900

    [✨feat]: 문제 풀이 페이지 추가

--- src/pages/ProblemSolvePage/ProblemSolvePage.tsx
+++ src/pages/ProblemSolvePage/ProblemSolvePage.tsx
@@ -1,3 +1,30 @@
+import { Button } from '@/components';
+import { useCustomTheme } from '@/hooks/useCustomTheme';
+import { useCompile, useSubmission } from '@/hooks/useProblemSolve';
+
+import * as S from './ProblemSolvePage.style';
+
 export default function ProblemSolvePage() {
-  return <div>ProblemSolvePage</div>;
+  const { theme } = useCustomTheme();
+
+  return (
+    <S.Wrapper>
+      <S.ContentsWrapper>
+        <S.ProblemWrapper>문제</S.ProblemWrapper>
+        <S.SolveWrapper>
+          <S.EditorWrapper>에디터</S.EditorWrapper>
+          <S.ExecutionWrapper>출력 및 실행 결과</S.ExecutionWrapper>
+        </S.SolveWrapper>
+      </S.ContentsWrapper>
+      <S.ButtonWrapper>
+        <Button
+          backgroundColor={theme.color.gray_20}
+          onClick={handleCompileExecution}
+        >
+          실행
+        </Button>
+        <Button onClick={handleSubmit}>제출</Button>
+      </S.ButtonWrapper>
+    </S.Wrapper>
+  );
 }
