f7ec1ffebb275637ca97cf515195d3e5c1faa14a
Cho-Ik-Jun <harry960629@naver.com>
2024-03-03 00:32:01 +0900

    [🎨design]: WelCome 페이지 메인 + 로그인 폼 디자인 레이아웃 구현

--- src/pages/WelcomePage/WelcomePage.style.ts
+++ src/pages/WelcomePage/WelcomePage.style.ts
@@ -1,6 +1,51 @@
 import styled, { css } from 'styled-components';
 
-export const WelcomePageWrapper = styled.div``;
+import { Col, Row } from '@/styles/GlobalStyle';
+
+export const WelcomePageWrapper = styled.div`
+  height: 100%;
+`;
+
+export const MainContainer = styled(Row)`
+  align-items: center;
+  justify-content: space-evenly;
+  height: 90vh;
+
+  > :last-of-type {
+    min-width: 30rem;
+  }
+`;
+
+export const MainLeftContainer = styled(Col)`
+  ${({ theme }) => css`
+    gap: ${theme.size.M};
+  `}
+`;
+
+export const MainTitleContainer = styled(Row)`
+  ${({ theme }) => css`
+    gap: ${theme.size.M};
+    align-items: center;
+  `}
+`;
+
+export const MainTitleText = styled.h1``;
+
+export const MainSubTitleText = styled.h3``;
+
+export const MainSubList = styled.ul``;
+
+export const MainSubListItem = styled.li``;
+
+export const MoreDetailContainer = styled.div`
+  height: fit-content;
+`;
+
+export const MoreDetailTitle = styled.p`
+  ${({ theme }) => css`
+    padding-left: ${theme.size.XXL};
+  `}
+`;
 
 export const DarkModeButtonContainer = styled.div`
   ${({ theme }) => css`
--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,13 +1,40 @@
-import { ThemeModeToggleButton } from '@/components';
+import { v4 } from 'uuid';
+
+import { Avatar, ThemeModeToggleButton } from '@/components';
 import LoginForm from '@/components/LoginForm/LoginForm';
 
 import * as S from './WelcomePage.style';
 
 export default function WelcomePage() {
+  // Todo: 아이콘 정하기
+  const mainSubItems = [
+    '⏲ 시간 및 공간 비용 최소화',
+    '⌨️ 실전 대비를 위한 모의 코딩테스트',
+    '💬 코드 풀이 공유 스터디',
+  ];
+
   return (
     <S.WelcomePageWrapper>
-      <h1>WelcomePage</h1>
-      <LoginForm />
+      <S.MainContainer>
+        <S.MainLeftContainer>
+          {/* 메인 컨테이너 로고 + 알고바로 */}
+          <S.MainTitleContainer>
+            {/* Todo: 로고 아이콘 넣기 */}
+            <Avatar size="L" />
+            <S.MainTitleText>AlgoBaro</S.MainTitleText>
+          </S.MainTitleContainer>
+          <S.MainSubTitleText>바로바로 함께 푸는 알고리즘</S.MainSubTitleText>
+          <S.MainSubList>
+            {mainSubItems.map(item => {
+              return <S.MainSubListItem key={v4()}>{item}</S.MainSubListItem>;
+            })}
+          </S.MainSubList>
+        </S.MainLeftContainer>
+        <LoginForm width="25%" />
+      </S.MainContainer>
+      <S.MoreDetailContainer>
+        <S.MoreDetailTitle>AlgoBaro가 궁금하신가요?</S.MoreDetailTitle>
+      </S.MoreDetailContainer>
       <S.DarkModeButtonContainer>
         <ThemeModeToggleButton></ThemeModeToggleButton>
       </S.DarkModeButtonContainer>
