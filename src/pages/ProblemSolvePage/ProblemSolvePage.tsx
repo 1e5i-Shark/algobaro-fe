6ee990fd1e2566e51cd83408999c5d16463c7f88
pySoo <soopy368@gmail.com>
2024-03-02 17:52:23 +0900

    [✨feat]: 문제 풀이 페이지의 mockData 생성, API hook 연동

--- src/pages/ProblemSolvePage/ProblemSolvePage.tsx
+++ src/pages/ProblemSolvePage/ProblemSolvePage.tsx
@@ -4,8 +4,32 @@ import { useCompile, useSubmission } from '@/hooks/useProblemSolve';
 
 import * as S from './ProblemSolvePage.style';
 
+const MOCK_DATA = {
+  COMPILE: {
+    language: 'java',
+    input: '1 2',
+    code: 'import java.io.BufferedReader;\nimport java.io.IOException;\nimport java.io.InputStreamReader;\n\npublic class Main {\n    public static void main(String[] args) throws IOException {\n        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));\n        String[] input = br.readLine().split(" ");\n        int a = Integer.parseInt(input[0]);\n        int b = Integer.parseInt(input[1]);\n        System.out.println(a + b);\n    }\n}',
+  },
+  SUBMISSION: {
+    roomUuid: '123e4567-e89b-12d3-a456-426614174000',
+    language: 'java',
+    code: 'import java.io.BufferedReader;\nimport java.io.IOException;\nimport java.io.InputStreamReader;\n\npublic class Main {\n    public static void main(String[] args) throws IOException {\n        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));\n        String[] input = br.readLine().split(" ");\n        int a = Integer.parseInt(input[0]);\n        int b = Integer.parseInt(input[1]);\n        System.out.println(a + b);\n    }\n}',
+    problemLink: 'https://www.acmicpc.net/problem/1000',
+  },
+};
+
 export default function ProblemSolvePage() {
   const { theme } = useCustomTheme();
+
+  const { mutate: compileMutate } = useCompile();
+  const { mutate: submitMutate } = useSubmission();
+
+  const handleCompileExecution = () => {
+    compileMutate(MOCK_DATA.COMPILE);
+  };
+  const handleSubmit = () => {
+    submitMutate(MOCK_DATA.SUBMISSION);
+  };
 
   return (
     <S.Wrapper>
