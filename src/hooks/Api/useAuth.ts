b1c6b5d15fabdd0880a69163f2121524c6ba05a1
Cho-Ik-Jun <harry960629@naver.com>
2024-03-03 00:44:32 +0900

    [♻️refactor]: useMutation 매개변수 코드 리팩토링

--- src/hooks/Api/useAuth.ts
+++ src/hooks/Api/useAuth.ts
@@ -6,22 +6,18 @@ import { PATH } from '@/routes/path';
 import { signIn } from '@/services/Auth';
 
 import { useLocalStorage } from '../useLocalStorage';
-import { SignInProps } from './type';
 
 /**
- * 로그인 signIn API 통신 함수를 사용하는 useMutation 훅이다.
+ * 로그인 signIn API 통신 함수를 사용하는 useMutation를 반환하는 훅이다.
  * - 로그인 성공 시 액세스 토큰을 로컬 스토리지에 저장한다.
  * - 저장 후 메인 페이지(홈)으로 다이렉팅한다.
- * @returns `mutate`, `data`
  */
 export const useSignIn = () => {
   const navigate = useNavigate();
   const [, setAccessToken] = useLocalStorage(LOCAL_ACCESSTOKEN);
 
   return useMutation({
-    mutationFn: ({ email, password }: SignInProps) => {
-      return signIn(email, password);
-    },
+    mutationFn: signIn,
     onSuccess: ({ response }) => {
       setAccessToken(response.accessToken);
       navigate(PATH.HOME);
