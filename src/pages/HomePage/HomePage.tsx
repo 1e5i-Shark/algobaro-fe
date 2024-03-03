0cec48862508fccfbafddfa07746cd0d4d67be20
타래 <85001878+meowTarae@users.noreply.github.com>
2024-03-03 16:21:38 +0900

    [✨feat]: nav 조건에 따른 필터 기능 구현

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,74 @@
+import { useEffect, useState } from 'react';
+
 import useRoomFilterStore from '@/store/useRoomFilterStore';
 
-import { DUMMY_DATA } from './DummyData';
+import { DUMMY_DATA, RoomDataProps } from './DummyData';
 import HomeNav from './HomeNav/HomeNav';
 import * as S from './HomePage.style';
 import HomeSection from './HomeSection/HomeSection';
 
 export default function HomePage() {
-  const { searchInputValue, selectedPrivate, selectedAccess } =
+  const [roomData, setRoomData] = useState<RoomDataProps[]>([]);
+  const [filteredData, setFilteredData] = useState<RoomDataProps[]>([]);
+  const { searchInputValue, selectedLanguage, selectedAccess, selectedStatus } =
     useRoomFilterStore();
-  console.log(searchInputValue, selectedPrivate, selectedAccess);
+
+  const hasOption =
+    searchInputValue ||
+    !!selectedLanguage.length ||
+    selectedAccess ||
+    selectedStatus;
+
+  // console.log(
+  //   searchInputValue,
+  //   !!selectedLanguage.length,
+  //   selectedAccess,
+  //   selectedStatus
+  // );
+  // console.log('filteredData : ', filteredData);
+
+  const filterData = () => {
+    let dataToFilter = roomData;
+    if (searchInputValue) {
+      setFilteredData(
+        filteredData.filter(data =>
+          data.title
+            .toLowerCase()
+            .includes(searchInputValue.toLowerCase().trim())
+        )
+      );
+    }
+    if (!!selectedLanguage.length) {
+      dataToFilter = dataToFilter.filter(data =>
+        selectedLanguage.every(lang => data.language.includes(lang))
+      );
+    }
+    if (selectedAccess) {
+      dataToFilter = dataToFilter.filter(
+        data => data.roomAccess === selectedAccess
+      );
+    }
+    if (selectedStatus) {
+      dataToFilter = dataToFilter.filter(data => data.roomStatus === '대기중');
+    }
+    setFilteredData(dataToFilter);
+  };
+
+  useEffect(() => {
+    if (!hasOption) {
+      // 아래 코드는 get 요청으로 수정될 예정입니다.
+      setRoomData(DUMMY_DATA);
+      return;
+    }
+
+    filterData();
+  }, [
+    hasOption,
+    searchInputValue,
+    selectedLanguage.length,
+    selectedAccess,
+    selectedStatus,
+  ]);
 
   return (
     <S.HomePageContainer>
@@ -18,7 +78,7 @@ export default function HomePage() {
 
         {/* 방 목록  */}
         <S.HomeSectionContainer>
-          {DUMMY_DATA.map(data => {
+          {(!hasOption ? roomData : filteredData).map(data => {
             return (
               <HomeSection
                 key={data.id}
