3ccd76b534cc629ed4cc8aa2b59b5ef0e75151de
pySoo <soopy368@gmail.com>
2024-03-04 00:25:06 +0900

    [🎨design]: Panel 영역에 맞춰 ProblemSolvePage 스타일링

--- src/pages/ProblemSolvePage/ProblemSolvePage.style.ts
+++ src/pages/ProblemSolvePage/ProblemSolvePage.style.ts
@@ -5,26 +5,22 @@ import { Col, Row } from '@/styles/GlobalStyle';
 const Wrapper = styled(Col)`
   width: 100%;
   height: 100%;
-  padding: 2rem 4rem;
 `;
 
 const ContentsWrapper = styled(Row)`
-  gap: 1rem;
   align-items: center;
   height: 100%;
 `;
 
 const ProblemWrapper = styled(Col)`
-  flex: 1;
   align-items: center;
   justify-content: center;
   height: 100%;
 `;
 
 const SolveWrapper = styled(Col)`
-  flex: 1;
-  gap: 1rem;
   align-items: center;
+  justify-content: center;
   height: 100%;
 `;
 
@@ -32,21 +28,26 @@ const EditorWrapper = styled(Col)`
   ${({ theme }) => css`
     align-items: center;
     justify-content: center;
-    width: 100%;
     height: 100%;
     background-color: ${theme.color.gray_50};
   `}
 `;
 
 const ExecutionWrapper = styled(Col)`
-  min-height: 20rem;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
 `;
 
 const ButtonWrapper = styled(Row)`
-  gap: 2rem;
-  align-items: center;
-  justify-content: end;
-  margin-top: 2.4rem;
+  ${({ theme }) => css`
+    gap: 2rem;
+    align-items: center;
+    justify-content: end;
+    padding: 1.4rem 2.5rem;
+    border-top: 2rem solid ${theme.color.background_menu};
+  `}
 `;
 
 export {
