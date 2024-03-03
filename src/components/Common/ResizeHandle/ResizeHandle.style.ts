8955148844ef467386d2fad3bb04cf3d08895e56
pySoo <soopy368@gmail.com>
2024-03-04 00:21:21 +0900

    [🎨design]: 방향에 따라서 ResizeHandle 스타일링, 아이콘 각도 조정

---
+++ src/components/Common/ResizeHandle/ResizeHandle.style.ts
@@ -0,0 +1,32 @@
+import { PanelResizeHandle } from 'react-resizable-panels';
+import styled, { css } from 'styled-components';
+
+import { DirectionType } from './ResizeHandle';
+
+interface ResizeHandleProps {
+  $direction: DirectionType;
+}
+
+const ResizeHandleWrapper = styled(PanelResizeHandle)<ResizeHandleProps>`
+  ${({ $direction, theme }) => css`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: ${$direction === 'horizontal' ? '0.2rem' : '100%'};
+    height: ${$direction === 'horizontal' ? '100%' : '0.2rem'};
+    padding: 1rem;
+    background-color: ${theme.color.background_menu};
+  `}
+`;
+
+const IconWrapper = styled.div<ResizeHandleProps>`
+  ${({ $direction }) => css`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%)
+      rotate(${$direction === 'horizontal' ? '90deg' : '0'});
+  `}
+`;
+
+export { IconWrapper, ResizeHandleWrapper };
