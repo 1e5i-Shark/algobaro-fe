89ac87c937924f739ec04e75c1d2431364cd74d1
Cho-Ik-Jun <harry960629@naver.com>
2024-03-04 13:14:16 +0900

    [✨feat]: 비밀번호 유효성 검사 조건 추가

--- src/constants/formValidation.ts
+++ src/constants/formValidation.ts
@@ -5,4 +5,18 @@ export const FORM_VALIDATION = {
       message: '이메일 형식이 아닙니다.',
     },
   },
+  PASSWORD: {
+    minLength: {
+      value: 8,
+      message: '최소 8자리 이상 입력해 주세요.',
+    },
+    maxLength: {
+      value: 20,
+      message: '최대 20자리 이내로 입력해 주세요.',
+    },
+    pattern: {
+      value: /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/,
+      message: '영문 숫자 특수 기호 조합 8 ~ 20자리로 구성해주세요.',
+    },
+  },
 };
