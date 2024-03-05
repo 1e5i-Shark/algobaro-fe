a171a732b32a60e8a464b7de993ee5949b050391
pySoo <soopy368@gmail.com>
2024-03-05 11:58:21 +0900

    [✨feat]: CreateTagInput의 EllipsisText 컴포넌트 제거

--- src/components/CreateRoom/CreateTagInput/CreateTagInput.tsx
+++ src/components/CreateRoom/CreateTagInput/CreateTagInput.tsx
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { useCustomTheme } from '@/hooks/useCustomTheme';
 
-import { EllipsisText, Tag } from '../..';
+import { Tag } from '../..';
 import * as S from './CreateTagInput.style';
 
 export interface TagType {
@@ -71,12 +71,7 @@ export default function CreateTagInput({
               fontSize={theme.size.M}
               textColor={theme.color.black_primary}
             >
-              <EllipsisText
-                width="30rem"
-                lineClamp={1}
-              >
-                {value}
-              </EllipsisText>
+              {value}
             </Tag>
           </S.TagItem>
         ))}
