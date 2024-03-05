a5fee983044fcd07cf4137dac416bba97a2d865c
Suyoung Shin <joyswim.dev@gmail.com>
2024-03-05 10:21:07 +0900

    [✨feat]: 방장위임, 강제퇴장을 처리하는 handleMenu 함수 추가

--- src/pages/RoomPage/RoomViews/MemberList/MemberList.tsx
+++ src/pages/RoomPage/RoomViews/MemberList/MemberList.tsx
@@ -1,8 +1,13 @@
+import { useMutation } from '@tanstack/react-query';
 import { useMemo } from 'react';
 
 import { MemberCard } from '@/components';
+import { MenuText } from '@/components/Common/Menu/MenuText';
+import { CardWrapper } from '@/components/MemberCard/MemberCard.style';
 import * as S from '@/pages/RoomPage/RoomPage.style';
+import { changeHost, ROOM_ROLE } from '@/services/Room/Room';
 import useRoomStore from '@/store/Room';
+import { MemberType } from '@/types/room';
 
 interface MemberListProps {
   className: string;
@@ -11,18 +16,71 @@ interface MemberListProps {
 const MAX_MEMBERS = 6;
 
 export default function MemberList({ className }: MemberListProps) {
-  const { roomData } = useRoomStore();
+  const { roomData, setRoomData } = useRoomStore();
 
   const { members } = roomData;
+  const [host] = members.filter(member => member.role === ROOM_ROLE.HOST);
+
+  const mutation = useMutation({
+    mutationFn: changeHost,
+    onSuccess: response => {
+      const { organizerId } = response;
+      // Todo: API 테스트
+      const updatedMembers: MemberType[] = members.map(member => {
+        if (member.id === organizerId) {
+          return { ...member, role: ROOM_ROLE.HOST };
+        } else if (member.role === ROOM_ROLE.HOST) {
+          return { ...member, role: ROOM_ROLE.MEMBER };
+        } else {
+          return member;
+        }
+      });
+
+      setRoomData({ members: updatedMembers });
+    },
+  });
+
+  const handleMenu = async (menu: string, memberId: number) => {
+    switch (menu) {
+      case MenuText.TransferHost:
+        // API 연결 후 삭제할 코드
+        const updatedMembers = members.map(member => {
+          if (member.id === host.id) {
+            return { ...member, role: ROOM_ROLE.MEMBER };
+          } else if (member.id === memberId) {
+            return { ...member, role: ROOM_ROLE.HOST };
+          } else {
+            return member;
+          }
+        });
+
+        setRoomData({ members: updatedMembers });
+        // Todo: 방장 수동 변경 API 테스트
+        // await mutation.mutate({ hostId: host.id, organizerId: memberId });
+        break;
+      case MenuText.KickOut:
+        const answer = confirm('정말 강제 퇴장하시겠습니까?');
+
+        if (answer) {
+          const newMembers = members.filter(member => member.id !== memberId);
+          setRoomData({ members: newMembers });
+        }
+        break;
+      default:
+        break;
+    }
+  };
 
   const renderCards = useMemo(() => {
     const renderedMembers = members.map(member => (
       <MemberCard
         key={member.id}
+        memberId={member.id}
         nickname={member.nickname}
         role={member.role}
         profileImage={member.profileImage}
         ready={member.ready}
+        onMenuClick={handleMenu}
       />
     ));
 
@@ -30,9 +88,9 @@ export default function MemberList({ className }: MemberListProps) {
 
     for (let i = 0; i < emptyCount; i++) {
       renderedMembers.push(
-        <MemberCard
+        <CardWrapper
           key={Date.now() + i}
-          isEmpty={true}
+          $isEmpty={true}
         />
       );
     }
