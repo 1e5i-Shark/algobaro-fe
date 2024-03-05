bbd3a6fe1d4f8fc1d0a3b2fcf82fa2a8f13f4a9b
Suyoung Shin <joyswim.dev@gmail.com>
2024-03-05 11:48:00 +0900

    [🚚rename]: 상수 코드 분리

---
+++ src/pages/RoomPage/RoomPage.consts.ts
@@ -0,0 +1,16 @@
+import { AccessType, RoleType, RoomStatusType } from '@/types/room';
+
+export const ROOM_STATUS: { [key: string]: RoomStatusType } = {
+  RECRUITING: 'RECRUITING',
+  RUNNING: 'RUNNING',
+};
+
+export const ROOM_ACCESS: { [key: string]: AccessType } = {
+  PUBLIC: 'PUBLIC',
+  PRIVATE: 'PRIVATE',
+};
+
+export const ROOM_ROLE: { [key: string]: RoleType } = {
+  HOST: 'HOST',
+  MEMBER: 'MEMBER',
+};
