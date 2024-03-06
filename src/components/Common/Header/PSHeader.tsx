6279723f6f217b5b7608fa066959237e2eaaf20f
pySoo <soopy368@gmail.com>
2024-03-06 11:36:17 +0900

    [✨feat]: 풀이 페이지 Confirm 모달 포기하기 버튼과 연결

--- src/components/Common/Header/PSHeader.tsx
+++ src/components/Common/Header/PSHeader.tsx
@@ -1,21 +1,26 @@
 import { useNavigate } from 'react-router-dom';
 
 import { useCustomTheme } from '@/hooks/useCustomTheme';
+import useModal from '@/hooks/useModal';
 import { PATH } from '@/routes/path';
 
 import Button from '../Button/Button';
+import Modal from '../Modal/Modal';
 import ThemeModeToggleButton from '../ThemeModeToggleButton/ThemeModeToggleButton';
 import * as S from './PSHeader.style';
 
 export default function PSHeader() {
   const { theme } = useCustomTheme();
+  const { modalRef, isOpen, openModal, closeModal } = useModal();
 
   const navigate = useNavigate();
 
+  const navigateToHome = () => {
+    navigate(PATH.HOME);
+  };
+
   const handleGiveUp = () => {
-    if (confirm('정말 포기하고 나가시겠습니까?')) {
-      navigate(PATH.HOME);
-    }
+    openModal();
   };
 
   return (
@@ -23,6 +28,7 @@ export default function PSHeader() {
       <S.TimerWrapper>타이머</S.TimerWrapper>
       <S.ButtonWrapper>
         <ThemeModeToggleButton />
+
         <Button
           height="4rem"
           backgroundColor={theme.color.red}
@@ -31,6 +37,37 @@ export default function PSHeader() {
           포기하기
         </Button>
       </S.ButtonWrapper>
+      <Modal
+        width="fit-content"
+        height="20rem"
+        ref={modalRef}
+        isOpen={isOpen}
+        onClose={closeModal}
+      >
+        <S.ConfirmModalWrapper>
+          <S.ConfirmText>정말 포기하고 나가시겠습니까?</S.ConfirmText>
+          <S.ConfirmButtonWrapper>
+            <Button
+              width="8rem"
+              height="4rem"
+              fontSize={theme.size.M}
+              backgroundColor={theme.color.red}
+              onClick={navigateToHome}
+            >
+              나가기
+            </Button>
+            <Button
+              width="8rem"
+              height="4rem"
+              fontSize={theme.size.M}
+              backgroundColor={theme.color.gray_20}
+              onClick={closeModal}
+            >
+              취소
+            </Button>
+          </S.ConfirmButtonWrapper>
+        </S.ConfirmModalWrapper>
+      </Modal>
     </S.HeaderWrapper>
   );
 }
