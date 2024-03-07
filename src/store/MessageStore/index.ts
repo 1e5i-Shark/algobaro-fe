ef6404df81bfdd810f17023fbe7ba227979908a7
Suyoung Shin <joyswim.dev@gmail.com>
2024-03-07 13:02:52 +0900

    [✨feat]: 소켓 MessageStore 추가
    Co-authored-by: 타래 <tarae7617@naver.com>

---
+++ src/store/MessageStore/index.ts
@@ -0,0 +1,152 @@
+import * as Stomp from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+import { v4 } from 'uuid';
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+import { chatType } from '@/constants/chat';
+import { LOCAL_ACCESSTOKEN } from '@/constants/localStorageKey';
+import { PATH } from '@/routes/path';
+import { sendMessageService } from '@/services/Message/sendMessageService';
+import { ChatValueUnion } from '@/types/chat';
+
+import { MessageStoreState, MessageStoreValue } from './type';
+
+const initialValue: MessageStoreValue = {
+  listeners: new Set<Function>(),
+  userId: v4(),
+  client: new Stomp.Client(),
+  connected: false,
+  roomIndices: [111, 222, 333],
+  currentRoomId: 0,
+  messageEntered: '',
+  messageLogs: [],
+  subscription: null,
+};
+
+const useMessageStore = create<MessageStoreState>()(
+  devtools((set, get) => ({
+    ...initialValue,
+    connect: roomId => {
+      const BASE_SOCKET_URL = import.meta.env.VITE_BASE_SOCKET_URL;
+      const socket = new SockJS(BASE_SOCKET_URL);
+      const stompClient = new Stomp.Client({
+        webSocketFactory: () => socket,
+        debug: str => {
+          console.log('stompClient debug string : ', str);
+        },
+        connectHeaders: {
+          Authorization: `Bearer ${localStorage.getItem(LOCAL_ACCESSTOKEN)}`,
+        },
+      });
+
+      stompClient.onConnect = () => {
+        const { subscribeMessageBroker, publish } = get();
+        set({ client: stompClient, currentRoomId: roomId });
+        subscribeMessageBroker(roomId);
+        set({ connected: true });
+        publish();
+      };
+      stompClient.activate();
+    },
+    subscribeMessageBroker: roomId => {
+      const client = get().client;
+      if (!client) return;
+
+      const subscription = client.subscribe(
+        `${PATH.SUBPREFIX}/chat/room/${roomId}`,
+        messageReceived => get().receiveMessage(messageReceived),
+        {
+          Authorization: `Bearer ${localStorage.getItem(LOCAL_ACCESSTOKEN)}`,
+        }
+      );
+
+      set({ subscription });
+      get().sendMessage(chatType.ENTER);
+    },
+    disconnect: () => {
+      const { client, subscription, sendMessage, publish } = get();
+      sendMessage(chatType.QUIT);
+
+      subscription?.unsubscribe();
+      client.deactivate();
+
+      set({
+        connected: false,
+        currentRoomId: 0,
+        messageEntered: '',
+        messageLogs: [],
+      });
+
+      publish();
+    },
+    changeInput: value => {
+      set({ messageEntered: value });
+      get().publish();
+    },
+    sendMessage: (type: ChatValueUnion) => {
+      const { client, currentRoomId, userId, messageEntered, publish } = get();
+      const message = type === chatType.MESSAGE ? messageEntered : '';
+
+      sendMessageService({
+        client,
+        type,
+        messageToSend: {
+          roomId: currentRoomId || 0,
+          userId: userId,
+          message,
+        },
+      });
+
+      set({ messageEntered: '' });
+      publish();
+    },
+    receiveMessage: messageReceived => {
+      const { formatMessage, publish } = get();
+      const message = JSON.parse(messageReceived.body);
+      set(state => ({
+        messageLogs: [...state.messageLogs, formatMessage(message)],
+      }));
+      publish();
+    },
+    formatMessage: message => {
+      const { type, userId, id, value, timestamp } = message;
+
+      switch (type) {
+        case chatType.ENTER:
+          return {
+            id,
+            value: `User ${userId} Enter (${timestamp})`,
+          };
+        case chatType.QUIT:
+          return {
+            id,
+            value: `User ${userId} Quit (${timestamp})`,
+          };
+        case chatType.MESSAGE:
+          return {
+            id,
+            value: `${userId}: ${value} (${timestamp})`,
+          };
+        default:
+          return {
+            id,
+            value: 'Unknown Type Message',
+          };
+      }
+    },
+    subscribe: listener => {
+      set(state => ({ listeners: new Set([...state.listeners, listener]) }));
+    },
+    unSubscribe: listener => {
+      set(state => ({
+        listeners: new Set([...state.listeners].filter(li => li !== listener)),
+      }));
+    },
+    publish() {
+      get().listeners.forEach(listener => listener());
+    },
+  }))
+);
+
+export default useMessageStore;
