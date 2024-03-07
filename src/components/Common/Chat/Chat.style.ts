7835bb74d559183087c0d4f98898af01431a56ca
타래 <85001878+meowTarae@users.noreply.github.com>
2024-03-07 19:40:17 +0900

    [🎨design]: textarea style 설정
    Co-authored-by: Suyoung Shin <joyswim.dev@gmail.com>

--- src/components/Common/Chat/Chat.style.ts
+++ src/components/Common/Chat/Chat.style.ts
@@ -3,7 +3,7 @@ import { css, styled } from 'styled-components';
 import { Col } from '@/styles/GlobalStyle';
 
 export const ChatContainer = styled(Col)`
-  height: 100vh;
+  height: 100%;
 `;
 
 export const MessagesContainer = styled(Col)`
@@ -20,38 +20,36 @@ export const MessageWrapper = styled.div`
 export const ChatInputContainer = styled.div`
   ${({ theme }) => css`
     display: flex;
-    align-content: center;
-    justify-content: center;
-    height: 6rem;
+    align-items: flex-end;
     border-top: ${`2px solid ${theme.color.transparent_50}`};
+
+    & > textarea {
+      flex-grow: 1;
+      padding: 1rem;
+      margin: 1rem;
+      background-color: ${theme.color.container_color};
+      border-radius: 0.5rem;
+    }
+
+    & > button {
+      margin: 1rem;
+    }
   `}
 `;
 
 export const InputWrapper = styled.div`
-  flex-grow: 1;
-  height: 100%;
-`;
-
-export const Form = styled.form`
-  width: 100%;
-  height: 100%;
-`;
-
-export const Input = styled.input`
   ${({ theme }) => css`
-    width: 100%;
+    flex-grow: 1;
     height: 100%;
-    padding: 0 1rem;
-    color: ${theme.color.text_primary_color};
-    background-color: ${theme.color.background_primary};
-  `}
-`;
 
-export const SendButton = styled.div`
-  display: flex;
-  align-items: center;
-  height: 100%;
-  margin: 0 1rem;
+    & > textarea {
+      width: 100%;
+      height: 100%;
+      padding: 0 1rem;
+      color: ${theme.color.text_primary_color};
+      background-color: ${theme.color.background_primary};
+    }
+  `}
 `;
 
 export const ChatListWrapper = styled.div`
