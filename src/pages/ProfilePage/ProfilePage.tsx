0940bd8439503fb854bd1419a17b7e23bdeb1651
Cho-Ik-Jun <harry960629@naver.com>
2024-03-08 19:31:21 +0900

    [✨feat]: 프로필 페이지 기본 레이아웃 및 데이터 표시

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,3 +1,81 @@
+import { Pagination } from '@mui/material';
+import { ChangeEvent, useState } from 'react';
+
+import { Avatar, Button, Tag } from '@/components';
+import { useCustomTheme } from '@/hooks/useCustomTheme';
+
+import { problemHistory } from './problemHistory';
+
 export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+  const { theme } = useCustomTheme();
+  const [pageNum, setPageNum] = useState(1);
+  const handlePageChange = (event: ChangeEvent<unknown>, value: number) => {
+    if (event) setPageNum(value);
+  };
+  return (
+    <div>
+      {/* 유저 정보 컨테이너 */}
+      <div>
+        <Avatar isEdit />
+        <div>
+          <p>알고나</p>
+          <p>algo@gmail.com</p>
+        </div>
+        <div>
+          <Button>수정</Button>
+          <Button>로그아웃</Button>
+        </div>
+      </div>
+      {/* 내가 푼 문제 */}
+      <div>
+        <p>내가 푼 문제</p>
+        <p>11</p>
+      </div>
+      {/* 풀이 히스토리 컨테이너 */}
+      <div>
+        <p>풀이 히스토리</p>
+        <ul>
+          {problemHistory.map(problem => {
+            return (
+              <li key={problem.id}>
+                <p>{problem.title}</p>
+                <Tag
+                  mode="normal"
+                  tagId={problem.languageId}
+                  backgroundColor={theme.color.gray_20}
+                >
+                  {problem.language}
+                </Tag>
+              </li>
+            );
+          })}
+        </ul>
+        <Pagination
+          count={10} // 표시할 페이지네이션 페이지 수
+          page={pageNum} // 현재 페이지 위치
+          siblingCount={1} // 현재 페이지 기준 양쪽 몇 개 표시
+          boundaryCount={0}
+          onChange={handlePageChange}
+          showFirstButton
+          showLastButton
+          size="large" //
+          color="secondary" //
+          sx={{
+            '& .MuiPaginationItem-root': {
+              color: theme.color.text_primary_color,
+              fontSize: theme.size.M,
+
+              '& .MuiSvgIcon-root': {
+                fontSize: theme.size.L,
+              },
+              '&.Mui-selected': {
+                backgroundColor: theme.color.secondary_color,
+                color: theme.color.background_primary,
+              },
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
 }
---
+++ src/pages/ProfilePage/problemHistory.ts
@@ -0,0 +1,68 @@
+export const problemHistory = [
+  {
+    id: 1,
+    title: '문제 1',
+    language: 'JavaScript',
+    languageId: 'javaScript',
+  },
+  {
+    id: 2,
+    title: '문제 2',
+    language: 'Python',
+    languageId: 'python',
+  },
+  {
+    id: 3,
+    title: '문제 3',
+    language: 'C++',
+    languageId: 'cPlusx2',
+  },
+  {
+    id: 4,
+    title: '문제 4',
+    language: 'Java',
+    languageId: 'java',
+  },
+  {
+    id: 5,
+    title: '문제 5',
+    language: 'Java',
+    languageId: 'java',
+  },
+  {
+    id: 6,
+    title: '문제 6',
+    language: 'Java',
+    languageId: 'java',
+  },
+  {
+    id: 7,
+    title: '문제 7',
+    language: 'Java',
+    languageId: 'java',
+  },
+  {
+    id: 8,
+    title: '문제 8',
+    language: 'Java',
+    languageId: 'java',
+  },
+  {
+    id: 9,
+    title: '문제 9',
+    language: 'Java',
+    languageId: 'java',
+  },
+  {
+    id: 10,
+    title: '문제 10',
+    language: 'Java',
+    languageId: 'java',
+  },
+  {
+    id: 11,
+    title: '문제 11',
+    language: 'Java',
+    languageId: 'java',
+  },
+];
