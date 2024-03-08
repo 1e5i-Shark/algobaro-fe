e90f5324b7f5c28b07c8539f05b5baba460b95e6
pySoo <soopy368@gmail.com>
2024-03-08 20:28:17 +0900

    [✨feat]: ProblemSolvePage에 CodeEditor 추가

--- src/pages/ProblemSolvePage/ProblemSolvePage.tsx
+++ src/pages/ProblemSolvePage/ProblemSolvePage.tsx
@@ -1,3 +1,9 @@
+import { CodeEditor } from '@/components';
+
 export default function ProblemSolvePage() {
-  return <div>ProblemSolvePage</div>;
+  return (
+    <>
+      <CodeEditor />
+    </>
+  );
 }
