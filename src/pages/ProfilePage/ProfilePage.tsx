e707293d0da5418b33cfab8d3c943f2489dc2bde
Cho-Ik-Jun <harry960629@naver.com>
2024-03-09 17:44:31 +0900

    [✨feat]: 로그아웃 버튼 기능 구현

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,23 +1,38 @@
 import { Pagination } from '@mui/material';
 import { ChangeEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Avatar, Button, Tag } from '@/components';
+import { LOCAL_ACCESSTOKEN } from '@/constants/localStorageKey';
 import { useMyInfo } from '@/hooks/Api/useMembers';
 import { useCustomTheme } from '@/hooks/useCustomTheme';
+import { useLocalStorage } from '@/hooks/useLocalStorage';
+import { PATH } from '@/routes/path';
 
 import { problemHistory } from './problemHistory';
 import * as S from './ProfilePage.style';
 
 export default function ProfilePage() {
   const { theme } = useCustomTheme();
+  // 로그아웃 버튼 기능을 위한 로컬스토리지 AccessToken set 함수를 가져온다.
+  const [, setAccessToken] = useLocalStorage(LOCAL_ACCESSTOKEN);
+  // 로그아웃 후 시작 페이지 이동을 위한 네비게이터 훅이다.
+  const navigate = useNavigate();
+  // 페이지네이션 상태 관리 함수이다.
   const [pageNum, setPageNum] = useState(1);
+  // 내 정보 가져오는 쿼리를 호출한다.
   const { data: myInfoData } = useMyInfo();
-
   const myInfo = myInfoData?.response;
 
   const handlePageChange = (event: ChangeEvent<unknown>, value: number) => {
     if (event) setPageNum(value);
   };
+  // 로그아웃 버튼 클릭 이벤트 핸들러 함수이다.
+  const handleClickLogOut = () => {
+    setAccessToken('');
+    navigate(PATH.ROOT);
+  };
+
   return (
     <S.ProfilePageWrapper>
       {/* 유저 정보 컨테이너 */}
@@ -34,7 +49,7 @@ export default function ProfilePage() {
         </S.UserInfoTextContainer>
         <S.UserInfoButtonContainer>
           <Button backgroundColor={theme.color.gray_20}>수정</Button>
-          <Button>로그아웃</Button>
+          <Button onClick={handleClickLogOut}>로그아웃</Button>
         </S.UserInfoButtonContainer>
       </S.UserInfoContainer>
       {/* 내가 푼 문제 */}
