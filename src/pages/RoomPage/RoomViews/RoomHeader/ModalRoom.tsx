8f81e30219f77d5c807e89a8fc9f547b7cc9ecce
suyoung <joyswim.dev@gmail.com>
2024-03-09 23:01:42 +0900

    [♻️refactor]: modalRoom PR 반영
    - mutation 주석처리
    - elseif문 if문으로 리팩토링
    - 반복코드 validateKeys 함수 분리
    - ModalButtonsWrapper 위치 수정

--- src/pages/RoomPage/RoomViews/RoomHeader/ModalRoom.tsx
+++ src/pages/RoomPage/RoomViews/RoomHeader/ModalRoom.tsx
@@ -1,11 +1,9 @@
-import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { Button, CheckBox, Input } from '@/components';
 import { useCustomTheme } from '@/hooks/useCustomTheme';
 import * as S from '@/pages/RoomPage/RoomPage.style';
-import { editRoom } from '@/services/Room/Room';
 import useRoomStore from '@/store/Room';
 import { AccessType } from '@/types/room';
 
@@ -21,7 +19,7 @@ interface InputProps {
 export default function ModalRoom({ onClose }: ModalRoomProps) {
   const { theme } = useCustomTheme();
   const { roomData, setRoomData } = useRoomStore();
-  const { roomId, problemLink, timeLimit, password, roomAccessType } = roomData;
+  const { problemLink, timeLimit, password, roomAccessType } = roomData;
 
   const [isPrivate, setIsPrivate] = useState(
     roomAccessType === 'PRIVATE' ? true : false
@@ -35,28 +33,21 @@ export default function ModalRoom({ onClose }: ModalRoomProps) {
     },
   });
 
-  const mutation = useMutation({
-    mutationFn: editRoom,
-    onSuccess: () => {
-      alert('mutation 방 수정 성공!');
-    },
-  });
+  // const mutation = useMutation({
+  //   mutationFn: editRoom,
+  //   onSuccess: () => {
+  //     alert('mutation 방 수정 성공!');
+  //   },
+  // });
 
   const onSubmit: SubmitHandler<InputProps> = data => {
     const newPassword = () => {
       const newAccessType: AccessType = isPrivate ? 'PRIVATE' : 'PUBLIC';
 
-      if (isPrivate) {
-        return {
-          password: data.password,
-          roomAccessType: newAccessType,
-        };
-      } else {
-        return {
-          password: '',
-          roomAccessType: newAccessType,
-        };
-      }
+      return {
+        password: isPrivate ? data.password : '',
+        roomAccessType: newAccessType,
+      };
     };
 
     const newData = {
@@ -82,25 +73,26 @@ export default function ModalRoom({ onClose }: ModalRoomProps) {
     onClose();
   };
 
+  const validateKeys = (
+    event: React.KeyboardEvent,
+    regex: RegExp,
+    allowedKeys: string[]
+  ) => {
+    if (!regex.test(event.key) && !allowedKeys.includes(event.key)) {
+      event.preventDefault();
+    }
+  };
+
   const allowedKeys = ['Backspace', 'Delete', 'ArrowLeft', 'ArrowRight', 'Tab'];
 
   const handleValidLink = (event: React.KeyboardEvent<HTMLInputElement>) => {
     const regex = /^[a-z0-9.:/]$/;
-
-    if (!regex.test(event.key) && !allowedKeys.includes(event.key)) {
-      event.preventDefault();
-    }
+    validateKeys(event, regex, allowedKeys);
   };
 
   const handleValidNumber = (event: React.KeyboardEvent<HTMLInputElement>) => {
     const regex = /^[0-9]$/;
-    if (!regex.test(event.key) && !allowedKeys.includes(event.key)) {
-      event.preventDefault();
-    }
-  };
-
-  const handleComplete = () => {
-    handleSubmit(onSubmit)();
+    validateKeys(event, regex, allowedKeys);
   };
 
   return (
@@ -140,22 +132,21 @@ export default function ModalRoom({ onClose }: ModalRoomProps) {
             />
           )}
         </S.PasswordWrapper>
+        <S.ModalButtonsWrapper>
+          <Button
+            type="submit"
+            width="30rem"
+          >
+            수정하기
+          </Button>
+          <Button
+            backgroundColor={theme.color.transparent_50}
+            onClick={onClose}
+          >
+            취소
+          </Button>
+        </S.ModalButtonsWrapper>
       </S.FormWrapper>
-      <S.ModalButtonsWrapper>
-        <Button
-          type="submit"
-          width="30rem"
-          onClick={handleComplete}
-        >
-          수정하기
-        </Button>
-        <Button
-          backgroundColor={theme.color.transparent_50}
-          onClick={onClose}
-        >
-          취소
-        </Button>
-      </S.ModalButtonsWrapper>
     </S.ModalWrapper>
   );
 }
