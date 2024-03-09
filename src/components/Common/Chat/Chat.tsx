6494b3e9032b019b5dc576551e3dce223ce13006
타래 <85001878+meowTarae@users.noreply.github.com>
2024-03-09 18:34:34 +0900

    [🐛fix]: 새로고침 시 자동 connect 안되던 버그 해결
    Co-authored-by: Suyoung Shin <joyswim.dev@gmail.com>

--- src/components/Common/Chat/Chat.tsx
+++ src/components/Common/Chat/Chat.tsx
@@ -1,7 +1,9 @@
 import { MouseEvent, useEffect, useRef } from 'react';
+import { useParams } from 'react-router-dom';
 import { v4 } from 'uuid';
 
 import { Message } from '@/components';
+import useMeStore from '@/store/Me';
 import useMessageStore from '@/store/MessageStore';
 
 import { MenuText } from '../Menu/MenuText';
@@ -14,7 +16,13 @@ interface ChatProps {
 
 export default function Chat({ height = '100%' }: ChatProps) {
   const scrollRef = useRef<HTMLDivElement>(null);
-  const { messageLogs } = useMessageStore();
+  const { me } = useMeStore();
+  // routes/index.ts 파일의 path에 정의된 :roomId을 가져옴
+  // * 현재 테스트의 경우 profile routes에서 진행되므로
+  // * routes/index.ts 파일의 42번째 라인의 userId를 roomId로 수정 후 테스트해야 정상 작동 됨.
+  const { roomId } = useParams();
+  const { messageLogs, connected, connect, setMessageValue, disconnect } =
+    useMessageStore();
 
   const menuList = [
     {
@@ -30,6 +38,21 @@ export default function Chat({ height = '100%' }: ChatProps) {
       scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
     }
   };
+
+  useEffect(() => {
+    const connectSocketServer = async (id: number) => {
+      if (connected) return;
+
+      setMessageValue({ userId: me.nickname });
+      connect(id);
+    };
+
+    roomId && connectSocketServer(+roomId);
+
+    return () => {
+      if (roomId && connected) disconnect();
+    };
+  }, [roomId, disconnect]);
 
   useEffect(() => {
     scrollToBottom();
