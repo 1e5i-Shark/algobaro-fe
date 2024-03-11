d88bc7a158754f4690a41070a0333ee58b1138ed
pySoo <soopy368@gmail.com>
2024-03-12 00:59:55 +0900

    [♻️refactor]: 상수를 외부 파일로 분리

--- src/pages/ProblemSolvePage/ProblemSolvePage.tsx
+++ src/pages/ProblemSolvePage/ProblemSolvePage.tsx
@@ -4,33 +4,8 @@ import { Button, ResizeHandle } from '@/components';
 import { useCustomTheme } from '@/hooks/useCustomTheme';
 import { useCompile, useSubmission } from '@/hooks/useProblemSolve';
 
+import { DIRECTION, MOCK_DATA, SIZE_PERCENTAGE } from './constants';
 import * as S from './ProblemSolvePage.style';
-
-const MOCK_DATA = {
-  COMPILE: {
-    language: 'java',
-    input: '1 2',
-    code: 'import java.io.BufferedReader;\nimport java.io.IOException;\nimport java.io.InputStreamReader;\n\npublic class Main {\n    public static void main(String[] args) throws IOException {\n        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));\n        String[] input = br.readLine().split(" ");\n        int a = Integer.parseInt(input[0]);\n        int b = Integer.parseInt(input[1]);\n        System.out.println(a + b);\n    }\n}',
-  },
-  SUBMISSION: {
-    roomUuid: '123e4567-e89b-12d3-a456-426614174000',
-    language: 'java',
-    code: 'import java.io.BufferedReader;\nimport java.io.IOException;\nimport java.io.InputStreamReader;\n\npublic class Main {\n    public static void main(String[] args) throws IOException {\n        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));\n        String[] input = br.readLine().split(" ");\n        int a = Integer.parseInt(input[0]);\n        int b = Integer.parseInt(input[1]);\n        System.out.println(a + b);\n    }\n}',
-    problemLink: 'https://www.acmicpc.net/problem/1000',
-  },
-};
-
-const DIRECTION = {
-  HORIZONTAL: 'horizontal',
-  VERTICAL: 'vertical',
-} as const;
-
-const SIZE_PERCENTAGE = {
-  PROBLEM: 40,
-  SOLVE: 60,
-  EDITOR: 80,
-  EXCEUTION: 20,
-} as const;
 
 export default function ProblemSolvePage() {
   const { theme } = useCustomTheme();
---
+++ src/pages/ProblemSolvePage/constants.ts
@@ -0,0 +1,25 @@
+export const MOCK_DATA = {
+  COMPILE: {
+    language: 'java',
+    input: '1 2',
+    code: 'import java.io.BufferedReader;\nimport java.io.IOException;\nimport java.io.InputStreamReader;\n\npublic class Main {\n    public static void main(String[] args) throws IOException {\n        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));\n        String[] input = br.readLine().split(" ");\n        int a = Integer.parseInt(input[0]);\n        int b = Integer.parseInt(input[1]);\n        System.out.println(a + b);\n    }\n}',
+  },
+  SUBMISSION: {
+    roomUuid: '123e4567-e89b-12d3-a456-426614174000',
+    language: 'java',
+    code: 'import java.io.BufferedReader;\nimport java.io.IOException;\nimport java.io.InputStreamReader;\n\npublic class Main {\n    public static void main(String[] args) throws IOException {\n        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));\n        String[] input = br.readLine().split(" ");\n        int a = Integer.parseInt(input[0]);\n        int b = Integer.parseInt(input[1]);\n        System.out.println(a + b);\n    }\n}',
+    problemLink: 'https://www.acmicpc.net/problem/1000',
+  },
+} as const;
+
+export const DIRECTION = {
+  HORIZONTAL: 'horizontal',
+  VERTICAL: 'vertical',
+} as const;
+
+export const SIZE_PERCENTAGE = {
+  PROBLEM: 40,
+  SOLVE: 60,
+  EDITOR: 80,
+  EXCEUTION: 20,
+} as const;
