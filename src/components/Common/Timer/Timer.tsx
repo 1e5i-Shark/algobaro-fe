1dca975359226b67157736356f93ec2d1bfe3b45
pySoo <soopy368@gmail.com>
2024-03-11 12:51:19 +0900

    [💬comment]: 공용 컴포넌트 Timer 주석 추가

--- src/components/Common/Timer/Timer.tsx
+++ src/components/Common/Timer/Timer.tsx
@@ -34,6 +34,17 @@ const convertedTime = (time: number, padLength: number) => {
   }
 };
 
+/**
+ * hh시간 mm분 ss초 형태의 시간을 보여주는 Timer 컴포넌트
+ * 분, 초 단위로 시간을 받아서 보여줍니다.
+ * padLength를 통해 자릿수를 변경할 수 있습니다.
+ * @param [minutes] - `옵션`
+ * @param [seconds] - `옵션`
+ * @param [padLength=2] - `옵션` 예시:) 01분 01초
+ * @param [openModal] - `옵션` 종료 후 모달을 열 때 사용합니다.
+ * @param [setIsEnd] - `옵션` 타이머 시간이 종료됨을 알려줍니다.
+ */
+
 export default function Timer({
   minutes = 0,
   seconds = 0,
