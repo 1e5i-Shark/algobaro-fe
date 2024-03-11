37dcd2dcdf7481fd578f6cffc1489d3027b2c01f
pySoo <soopy368@gmail.com>
2024-03-11 12:52:16 +0900

    [✨feat]: 타이머 종료 모달에서 공용 Timer 사용으로 이동 전까지 남은 시간 표기

---
+++ src/pages/ProblemSolvePage/ProblemEndModal/ProblemEndModal.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+import { Modal, Timer } from '@/components';
+
+import * as S from './ProblemEndModal.style';
+
+interface ProblemEndModalProps {
+  modalRef: React.RefObject<HTMLDivElement>;
+  isOpen: boolean;
+  closeModal: () => void;
+}
+
+export default function ProblemEndModal({
+  modalRef,
+  isOpen,
+  closeModal,
+}: ProblemEndModalProps) {
+  const [isEnd, setIsEnd] = useState(false);
+
+  return (
+    <Modal
+      mode="confirm"
+      width="fit-content"
+      height="20rem"
+      ref={modalRef}
+      isOpen={isOpen}
+      onClose={closeModal}
+    >
+      <S.Wrapper>
+        <S.ModalTitle>시험이 종료 되었습니다.</S.ModalTitle>
+        {!isEnd && (
+          <S.InfoTitle>
+            <span>{`풀이 공유 페이지로 `}</span>
+            <Timer
+              seconds={5}
+              padLength={0}
+              setIsEnd={setIsEnd}
+            />
+            <span>{` 뒤에 이동합니다..`}</span>
+          </S.InfoTitle>
+        )}
+      </S.Wrapper>
+    </Modal>
+  );
+}
