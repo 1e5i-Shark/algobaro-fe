44c8374839e2f1156518a6c0de22d6a3b92e854a
pySoo <soopy368@gmail.com>
2024-03-11 12:52:49 +0900

    [✨feat]: 문제 풀이 페이지 타이머, 모달 연동

--- src/pages/ProblemSolvePage/ProblemSolvePage.tsx
+++ src/pages/ProblemSolvePage/ProblemSolvePage.tsx
@@ -1,37 +1,23 @@
-import { Button, Modal } from '@/components';
-import Timer from '@/components/ProblemSolve/Timer/Timer';
-import { useCustomTheme } from '@/hooks/useCustomTheme';
 import useModal from '@/hooks/useModal';
 
+import ProblemEndModal from './ProblemEndModal/ProblemEndModal';
 import * as S from './ProblemSolvePage.style';
+import ProblemTimer from './ProblemTimer/ProblemTimer';
 
 export default function ProblemSolvePage() {
-  const { theme } = useCustomTheme();
   const { modalRef, isOpen, openModal, closeModal } = useModal();
 
   return (
     <S.Wrapper>
-      <Timer
+      <ProblemTimer
         seconds={5}
         openModal={openModal}
       />
-      <Modal
-        width="fit-content"
-        height="20rem"
-        ref={modalRef}
+      <ProblemEndModal
+        modalRef={modalRef}
         isOpen={isOpen}
-        onClose={closeModal}
-      >
-        <S.ModalWrapper>
-          <S.ModalTitle>시험이 종료 되었습니다.</S.ModalTitle>
-          <Button
-            width="100%"
-            fontSize={theme.size.M}
-          >
-            풀이 공유 페이지로 이동
-          </Button>
-        </S.ModalWrapper>
-      </Modal>
+        closeModal={closeModal}
+      />
     </S.Wrapper>
   );
 }
