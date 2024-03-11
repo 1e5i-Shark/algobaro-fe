5a2008b4bd510a2ef8b7556ff3e5ef61fd130af4
Cho-Ik-Jun <harry960629@naver.com>
2024-03-11 20:46:30 +0900

    [✨feat]: 문제 풀이 히스토리 API 연동 및 페이지네이션 기능 구현
    - 문제 풀이 히스토리 기능 API 연동까지 모두 구현
    - 로딩 시 스피너 추가
    - 빈 히스토리 영역 추가
    - 전체 상세 주석 추가

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,40 +1,62 @@
 import { Pagination } from '@mui/material';
-import { ChangeEvent, useRef, useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Avatar, Button, Tag } from '@/components';
+import { Avatar, Button, Spinner, Tag } from '@/components';
 import { LOCAL_ACCESSTOKEN } from '@/constants/localStorageKey';
 import { useEditMyImage, useMyInfo } from '@/hooks/Api/useMembers';
+import { useSolvedHistoryList } from '@/hooks/Api/useSolves';
 import { useCustomTheme } from '@/hooks/useCustomTheme';
 import { useLocalStorage } from '@/hooks/useLocalStorage';
 import { PATH } from '@/routes/path';
 
-import { problemHistoryResponse } from './problemHistoryResponse';
 import * as S from './ProfilePage.style';
 
 export default function ProfilePage() {
   const { theme } = useCustomTheme();
 
+  // 로그아웃 버튼 기능을 위한 로컬스토리지 AccessToken set 함수를 가져온다.
+  const [, setAccessToken] = useLocalStorage(LOCAL_ACCESSTOKEN);
+  // 로그아웃 후 시작 페이지 이동을 위한 네비게이터 훅
+  const navigate = useNavigate();
+
+  // 아바타 이미지 변경을 위한 input ref와 avatarImage state이다.
   const fileInput = useRef<HTMLInputElement>(null);
   const [avatarImage, setAvatarImage] = useState('');
 
-  const { mutate: editMyImageMutate } = useEditMyImage();
-
-  // 로그아웃 버튼 기능을 위한 로컬스토리지 AccessToken set 함수를 가져온다.
-  const [, setAccessToken] = useLocalStorage(LOCAL_ACCESSTOKEN);
-  // 로그아웃 후 시작 페이지 이동을 위한 네비게이터 훅이다.
-  const navigate = useNavigate();
-  // 페이지네이션 상태 관리 함수이다.
-  const [pageNum, setPageNum] = useState(0);
+  // 정보 및 암호 변경 모달 open state이다.
   const [isOpenEditInfoModal, setIsOpenEditInfoModal] = useState(false);
   const [isOpenEditPWModal, setIsOpenEditPWModal] = useState(false);
-  // 내 정보 가져오는 쿼리를 호출한다.
+
+  // 페이지네이션 상태 관리 함수
+  const [pageNum, setPageNum] = useState(0);
+
+  // 내 정보 가져오는 useQuery 훅
   const { data: myInfoData } = useMyInfo();
   const myInfo = myInfoData?.response;
 
-  const totalPageNum = problemHistoryResponse[0].totalPages;
-  const totalHistoryNum = problemHistoryResponse[0].totalElements;
+  // 풀이 히스토리를 가져오는 useQuery 훅
+  // 한 페이지에 4개의 히스토리를 가져온다.
+  const {
+    data: solvedHistoryData,
+    isLoading,
+    refetch: fetchSolvedHistory,
+  } = useSolvedHistoryList({
+    page: pageNum,
+    size: 4,
+  });
 
+  const solvedHistoryRes = solvedHistoryData?.response;
+  // 풀이 히스토리 리스트 데이터
+  const solvedHistoryList = solvedHistoryRes?.content;
+  // 전체 페이지 수
+  const totalPageNum = solvedHistoryRes?.totalPages;
+  // 전체 히스토리 데이터 수
+  const totalHistoryNum = solvedHistoryRes?.totalElements;
+  // 프로필 이미지 변경하는 API useMutation 훅
+  const { mutate: editMyImageMutate } = useEditMyImage();
+
+  // 페이지 번호를 클릭하여 변경하는 이벤트 핸들러 함수
   const handlePageChange = (event: ChangeEvent<unknown>, value: number) => {
     if (event) setPageNum(value - 1);
   };
@@ -44,13 +66,13 @@ export default function ProfilePage() {
     fileInput.current?.click();
   };
 
+  // 사용자가 프로필 이미지를 선택하여 변경하면 API로 formData를 전송한다.
   const handleChangeAvatar = (e: ChangeEvent<HTMLInputElement>) => {
     const formData = new FormData();
     const fileData = e.target.files?.[0];
     const fileUrl = fileData ? URL.createObjectURL(fileData) : '';
     const fileSize = fileData?.size || 0;
-    // TODO: 이후 5MB로 변경
-    const limitSize = 1024 ** 2 * 1; // 5MB 용량 제한
+    const limitSize = 1024 ** 2 * 5; // 5MB 용량 제한
 
     // 용량 제한 경고를 표시한다.
     if (fileSize >= limitSize) alert('5MB 이하 이미지만 업로드 가능합니다!');
@@ -84,10 +106,16 @@ export default function ProfilePage() {
     setIsOpenEditPWModal(false);
   };
 
+  // 페이지 번호가 바뀌면 문제 히스토리를 refetch 한다.
+  useEffect(() => {
+    fetchSolvedHistory();
+  }, [pageNum]);
+
   return (
     <S.ProfilePageWrapper>
       {/* 유저 정보 컨테이너 */}
       <S.UserInfoContainer>
+        {/* 아바타 프로필 사진 */}
         <S.UserAvatarContainer>
           <S.AvatarInput
             name="avatarImage"
@@ -104,10 +132,12 @@ export default function ProfilePage() {
             src={myInfo?.profileImage || avatarImage}
           />
         </S.UserAvatarContainer>
+        {/* 닉네임, 이메일 */}
         <S.UserInfoTextContainer>
           <S.UserName>{myInfo ? myInfo.nickname : ''}</S.UserName>
           <S.UserEmail>{myInfo ? myInfo.email : ''}</S.UserEmail>
         </S.UserInfoTextContainer>
+        {/* 정보, 암호 수정 및 로그아웃 버튼 */}
         <S.UserInfoButtonContainer>
           <Button
             onClick={handleClickEditInfoBtn}
@@ -126,63 +156,87 @@ export default function ProfilePage() {
       </S.UserInfoContainer>
       {/* 내가 푼 문제 */}
       <S.MySolveTextContainer>
-        <p>내가 푼 문제</p>
+        <p>내가 푼 문제: </p>
         <p>{totalHistoryNum}</p>
       </S.MySolveTextContainer>
       {/* 풀이 히스토리 컨테이너 */}
-      {/* TODO: 히스토리 하나도 없는 빈 히스토리 컨테이너 추가 */}
       <div>
         <p>풀이 히스토리</p>
-        <ul>
-          {problemHistoryResponse[pageNum].content.map(problem => {
-            return (
-              <li key={problem.id}>
-                {/* TODO: 응답에 문제 링크 추가되면 변경 */}
-                <p>{problem.roomUuid}</p>
-                <Tag
-                  mode="normal"
-                  tagId={problem.language || 'null'}
-                  backgroundColor={theme.color.gray_20}
-                  textColor={theme.color.black_primary}
-                >
-                  {problem.language}
-                </Tag>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <Pagination
-        count={totalPageNum} // 표시할 페이지네이션 페이지 수
-        page={pageNum + 1} // 현재 페이지 위치
-        siblingCount={1} // 현재 페이지 기준 양쪽 몇 개 표시
-        boundaryCount={0}
-        onChange={handlePageChange}
-        showFirstButton
-        showLastButton
-        size="large" //
-        color="secondary" //
-        sx={{
-          '& .MuiPaginationItem-root': {
-            color: theme.color.text_primary_color,
-            fontSize: theme.size.M,
 
-            '& .MuiSvgIcon-root': {
-              fontSize: theme.size.L,
+        {/* TODO: 히스토리 하나도 없는 빈 히스토리 컨테이너 추가 */}
+        {isLoading ? (
+          <Spinner />
+        ) : solvedHistoryList?.length !== 0 ? (
+          <ul>
+            {solvedHistoryList?.map(problem => {
+              return (
+                <li key={problem.id}>
+                  {/* TODO: 응답에 문제 링크 추가되면 변경 */}
+                  <p>{problem.problemLink}</p>
+                  <Tag
+                    mode="normal"
+                    tagId={problem.solveStatus || 'null'}
+                    backgroundColor={
+                      problem.solveStatus === 'SUCCESS'
+                        ? theme.color.green
+                        : theme.color.red
+                    }
+                    textColor={theme.color.black_primary}
+                  >
+                    {problem.solveStatus}
+                  </Tag>
+                  <Tag
+                    mode="normal"
+                    tagId={problem.language || 'null'}
+                    backgroundColor={theme.color.gray_20}
+                    textColor={theme.color.black_primary}
+                  >
+                    {problem.language}
+                  </Tag>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <div>터엉</div>
+        )}
+        {/* 페이지네이션 영역 */}
+        <Pagination
+          count={totalPageNum} // 표시할 페이지네이션 페이지 수
+          // TODO: Merge 시 삭제
+          // count={5} // 빈 페이지 테스트용 코드
+          page={pageNum + 1} // 현재 페이지 위치
+          siblingCount={1} // 현재 페이지 기준 양쪽 몇 개 표시
+          boundaryCount={0}
+          onChange={handlePageChange}
+          showFirstButton
+          showLastButton
+          size="large" //
+          color="secondary" //
+          sx={{
+            '& .MuiPaginationItem-root': {
+              color: theme.color.text_primary_color,
+              fontSize: theme.size.M,
+
+              '& .MuiSvgIcon-root': {
+                fontSize: theme.size.L,
+              },
+              '&.Mui-selected': {
+                backgroundColor: theme.color.secondary_color,
+                color: theme.color.background_primary,
+              },
             },
-            '&.Mui-selected': {
-              backgroundColor: theme.color.secondary_color,
-              color: theme.color.background_primary,
-            },
-          },
-        }}
-      />
+          }}
+        />
+      </div>
+      {/* 정보 수정 모달 */}
       <S.EditInfoModal
         isOpen={isOpenEditInfoModal}
         onClose={handleCloseEditInfoModal}
         width="60%"
         height="60%"
       />
+      {/* 암호 변경 모달 */}
       <S.EditPWModal
         isOpen={isOpenEditPWModal}
         onClose={handleCloseEditPWModal}
