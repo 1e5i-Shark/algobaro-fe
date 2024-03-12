67fe3cc42386c530c2c683c18e2179ebde55b682
Suyoung Shin <joyswim.dev@gmail.com>
2024-03-12 10:23:13 +0900

    [♻️refactor]: status 변환하는 객체를 함수로 리팩토링 및 role 상수 교체

--- src/components/MemberCard/StatusTag.tsx
+++ src/components/MemberCard/StatusTag.tsx
@@ -4,6 +4,7 @@ import SportsRoundedIcon from '@mui/icons-material/SportsRounded';
 
 import { Icon } from '@/components';
 import { useCustomTheme } from '@/hooks/useCustomTheme';
+import { ROOM_ROLE } from '@/pages/RoomPage/RoomPage.consts';
 import { RoleType } from '@/types/room';
 
 import * as S from './MemberCard.style';
@@ -16,25 +17,27 @@ interface StatusTagProps {
 export default function StatusTag({ role, ready }: StatusTagProps) {
   const { theme } = useCustomTheme();
 
-  const statusToText = {
-    HOST: '방장',
-    MEMBER: (ready: boolean) => (ready ? '준비 완료' : '대기중'),
+  const convertStatus = (role: string, ready: boolean) => {
+    if (role === ROOM_ROLE.HOST) return '방장';
+    if (ready) return '준비 완료';
+    return '대기중';
   };
+  const status = convertStatus(role, ready);
 
   return (
     <S.StatusWrapper>
       <S.StatusText
         $color={
-          role === 'HOST'
+          role === ROOM_ROLE.HOST
             ? theme.color.text_primary_color
             : ready
               ? theme.color.green
               : theme.color.red
         }
       >
-        {role === 'HOST' ? statusToText[role] : statusToText[role](ready)}
+        {status}
       </S.StatusText>
-      {role === 'HOST' ? (
+      {role === ROOM_ROLE.HOST ? (
         <Icon color={theme.color.text_primary_color}>
           <SportsRoundedIcon />
         </Icon>
