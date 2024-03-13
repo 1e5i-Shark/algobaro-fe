23559b9ff0ace45a5e73d5c2f92f1c32501da793
Suyoung Shin <joyswim.dev@gmail.com>
2024-03-13 14:33:40 +0900

    [♻️refactor]: 쿼리 훅 분리

--- src/hooks/useRooms.ts
+++ src/hooks/useRooms.ts
@@ -1,9 +1,18 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
+import { QUERY_KEY } from '@/constants/queryKey';
 import { createRoom } from '@/services/Room/createRoom';
+import { getUuidRoom } from '@/services/Room/Room';
 
 export const useCreateRoom = () => {
   return useMutation({
     mutationFn: createRoom,
+  });
+};
+
+export const useGetUuidRoom = (roomShortUuid: number) => {
+  return useQuery({
+    queryKey: [QUERY_KEY.ROOM.UUID_INFO, roomShortUuid],
+    queryFn: async () => await getUuidRoom(`/${roomShortUuid}`),
   });
 };
