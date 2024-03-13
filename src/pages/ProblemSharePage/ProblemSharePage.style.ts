47aa1050765d58ae5a368ec0656e4e476557e35c
pySoo <soopy368@gmail.com>
2024-03-13 13:34:44 +0900

    [🎨design]: 코드 공유 페이지 css 적용

---
+++ src/pages/ProblemSharePage/ProblemSharePage.style.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  padding: 4rem 2rem;
+`;
+
+const CodeEditorWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  margin-top: 2rem;
+`;
+
+export { CodeEditorWrapper, Wrapper };
