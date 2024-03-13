ad3384b8717fa4beaa1356375c2b8a505d07d3f4
pySoo <soopy368@gmail.com>
2024-03-13 13:33:38 +0900

    [✨feat]: UserProfileList 컴포넌트 추가

---
+++ src/pages/ProblemSharePage/UserProfileList/UserProfileList.tsx
@@ -0,0 +1,42 @@
+import { Avatar, EllipsisText } from '@/components';
+
+import { User } from '../type';
+import * as S from './UserProfileList.style';
+
+interface UserProfileListProps {
+  selectedUser: User;
+  userList: User[];
+  onUserClick: (userId: string) => void;
+}
+
+export default function UserProfileList({
+  selectedUser,
+  userList,
+  onUserClick,
+}: UserProfileListProps) {
+  return (
+    <S.Wrapper>
+      <S.UserList>
+        {userList.map(user => (
+          <S.UserItem
+            key={user.id}
+            onClick={() => onUserClick(user.id)}
+          >
+            <Avatar
+              size="L"
+              src={user.profileImage ?? ''}
+            />
+            <S.UserName $isSelected={selectedUser.id === user.id}>
+              <EllipsisText
+                width="8rem"
+                lineClamp={2}
+              >
+                {user.nickname}
+              </EllipsisText>
+            </S.UserName>
+          </S.UserItem>
+        ))}
+      </S.UserList>
+    </S.Wrapper>
+  );
+}
