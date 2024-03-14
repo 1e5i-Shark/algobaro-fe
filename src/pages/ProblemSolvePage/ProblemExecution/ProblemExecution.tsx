ec11d905ef68fc3e0a2a363e1f8ffd185c4f899d
pySoo <soopy368@gmail.com>
2024-03-14 16:20:48 +0900

    [✨feat]: compile API 데이터에 따라 출력, loading 시에 Spinner를 보여주기

--- src/pages/ProblemSolvePage/ProblemExecution/ProblemExecution.tsx
+++ src/pages/ProblemSolvePage/ProblemExecution/ProblemExecution.tsx
@@ -1,5 +1,32 @@
+import { Spinner } from '@/components';
+import { useCustomTheme } from '@/hooks/useCustomTheme';
+import useCodeEditorStore from '@/store/CodeEditorStore';
+
 import * as S from './ProblemExecution.style';
 
-export default function ProblemExecution() {
-  return <S.Wrapper>실행 결과가 표시됩니다.</S.Wrapper>;
+interface Props {
+  isLoading: boolean;
+}
+
+export default function ProblemExecution({ isLoading }: Props) {
+  const { theme } = useCustomTheme();
+  const result = useCodeEditorStore(state => state.result);
+
+  return (
+    <S.Wrapper>
+      <S.ResultTitle>실행 결과</S.ResultTitle>
+      <S.ResultWrapper>
+        {isLoading && <Spinner color={theme.color.gray_30} />}
+        {!isLoading && (
+          <S.ResultText>
+            {result === '' ? (
+              <S.GuideText>실행 버튼을 누르면 결과가 표시됩니다.</S.GuideText>
+            ) : (
+              result
+            )}
+          </S.ResultText>
+        )}
+      </S.ResultWrapper>
+    </S.Wrapper>
+  );
 }
