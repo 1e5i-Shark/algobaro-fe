f9682108305043e53e97658671346a0781f64c90
pySoo <soopy368@gmail.com>
2024-03-14 16:46:52 +0900

    [✨feat]: compile, submission API 연동, roomUuid와 problemLink는 Mock data로 설정

--- src/pages/ProblemSolvePage/ProblemSolvePage.tsx
+++ src/pages/ProblemSolvePage/ProblemSolvePage.tsx
@@ -1,11 +1,12 @@
 import { Panel, PanelGroup } from 'react-resizable-panels';
 
 import { Button, CodeEditor, ResizeHandle } from '@/components';
+import { MOCK_ROOM_DATA } from '@/constants/room';
 import { useCustomTheme } from '@/hooks/useCustomTheme';
 import { useCompile, useSubmission } from '@/hooks/useProblemSolve';
-import useTimerStore from '@/store/TimerStore';
+import useCodeEditorStore from '@/store/CodeEditorStore';
 
-import { DIRECTION, MOCK_DATA, SIZE_PERCENTAGE } from './constants';
+import { DIRECTION, SIZE_PERCENTAGE } from './constants';
 import ProblemExecution from './ProblemExecution/ProblemExecution';
 import ProblemSection from './ProblemSection/ProblemSection';
 import * as S from './ProblemSolvePage.style';
@@ -13,18 +14,28 @@ import * as S from './ProblemSolvePage.style';
 export default function ProblemSolvePage() {
   const { theme } = useCustomTheme();
 
-  const { mutate: compileMutate } = useCompile();
+  // TODO: roomStore에서 가져오도록 수정
+  const roomShortUuid = MOCK_ROOM_DATA.roomShortUuid;
+
+  // TODO: 문제 링크를 가져오도록 수정
+  const MOCK_PROBLEM_LINK = 'https://www.acmicpc.net/problem/1000';
+
+  const { mutate: compileMutate, isLoading: isCompileLoading } = useCompile();
   const { mutate: submitMutate } = useSubmission();
 
-  const setIsStop = useTimerStore(state => state.setIsStop);
+  const { input, code, language } = useCodeEditorStore(state => state);
 
-  const handleCompileExecution = () => {
-    compileMutate(MOCK_DATA.COMPILE);
+  const handleCompileExecution = async () => {
+    compileMutate({ code, input, language });
   };
 
   const handleSubmit = () => {
-    submitMutate(MOCK_DATA.SUBMISSION);
-    setIsStop(false);
+    submitMutate({
+      roomShortUuid,
+      language,
+      code,
+      problemLink: MOCK_PROBLEM_LINK,
+    });
   };
 
   return (
@@ -47,7 +58,7 @@ export default function ProblemSolvePage() {
               <ResizeHandle direction={DIRECTION.VERTICAL} />
               <Panel defaultSize={SIZE_PERCENTAGE.EXECUTION}>
                 {/* 실행 영역 */}
-                <ProblemExecution />
+                <ProblemExecution isLoading={isCompileLoading} />
               </Panel>
             </PanelGroup>
           </Panel>
