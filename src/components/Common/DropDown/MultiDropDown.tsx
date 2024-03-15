754719c1b45602b25f3fee5dd50d29abf7c70b53
타래 <85001878+meowTarae@users.noreply.github.com>
2024-03-15 17:25:06 +0900

    [💄style]: MultiDropdown 모든 필터 선택 시  전체 언어 => 모든 언어 텍스트 수정

--- src/components/Common/DropDown/MultiDropDown.tsx
+++ src/components/Common/DropDown/MultiDropDown.tsx
@@ -105,7 +105,7 @@ export default function MultiDropDown({
           value={selectedValues}
           renderValue={selected =>
             selected.length === dataSetLength
-              ? '전체 언어'
+              ? '모든 언어'
               : `${selected.length}개 선택`
           }
           onChange={handleChange}
