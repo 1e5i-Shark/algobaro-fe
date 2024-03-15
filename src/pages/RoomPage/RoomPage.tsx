834351b6a649242cf1d9ad91393fc668cc612287
Suyoung Shin <joyswim.dev@gmail.com>
2024-03-15 22:58:05 +0900

    [♻️refactor]: 참여 인원이 추가되었을 때 listeners 감지 후 refetch

--- src/pages/RoomPage/RoomPage.tsx
+++ src/pages/RoomPage/RoomPage.tsx
@@ -11,14 +11,13 @@ import useRoomStore from '@/store/RoomStore';
 import { findMyRoomData } from './findMyRoomData';
 import MemberList from './MemberList/MemberList';
 import RoomHeader from './RoomHeader/RoomHeader';
-import { ROOM_ROLE } from './RoomPage.consts';
 import * as S from './RoomPage.style';
 import TestInfo from './TestInfo/TestInfo';
 
 export default function RoomPage() {
   const { myRoomData, setMyRoomData, setRoomData } = useRoomStore();
   const { myInfo } = useMyInfoStore();
-  const { connected, connect, disconnect } = useMessageStore();
+  const { connected, listeners, connect, disconnect } = useMessageStore();
 
   const { roomShortUuid } = useParams();
 
@@ -33,13 +32,15 @@ export default function RoomPage() {
     // 소켓 초기 연결
     if (!connected) {
       connect(roomShortUuid);
-      refetch();
     } else {
       disconnect();
     }
-    // 참여인원이 추가되었으므로 다시 refetch
   }, []);
 
+  // 참여인원이 추가되었으므로 다시 refetch
+  useEffect(() => {
+    refetch();
+  }, [listeners]);
   // Todo: 새로고침했을 때 다시 connect하는 대응
   // const reconnectSocketServer = (id: string) => {
   //   connect(id);
@@ -51,7 +52,6 @@ export default function RoomPage() {
   //   } else {
   //     disconnect();
   //   }
-
   // }, []);
 
   if (isError) {
@@ -65,18 +65,6 @@ export default function RoomPage() {
     if (isSuccess && data.response) {
       const { roomMembers: roomMembersData } = data.response;
       setRoomData(data.response);
-
-      const date = new Date().toISOString();
-      setMyRoomData({
-        memberId: myInfo.id as number,
-        email: myInfo.email,
-        nickname: myInfo.nickname,
-        profileImage: myInfo.profileImage,
-        role:
-          roomMembersData.length >= 1 ? ROOM_ROLE.PARTICIPANT : ROOM_ROLE.HOST,
-        joinTime: date,
-        ready: false,
-      });
 
       if (!myInfo.id) return;
 
@@ -92,9 +80,9 @@ export default function RoomPage() {
    * 그러나 비정상적인 네트워크 종료 등으로 브라우저가 갑작스럽게 연결이 끊기면 서버 측에서 이를 감지하기 어려울 수 있다.
    * 따라서 beforeUnloadListener로 명시적으로 웹소켓 연결을 끊어준다.
    */
-  const beforeUnloadListener = async () => {
+  const beforeUnloadListener = () => {
     if (connected) {
-      await disconnect();
+      disconnect();
     }
   };
 
