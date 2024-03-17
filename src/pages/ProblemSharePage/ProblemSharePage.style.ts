8ac5edf888f3cd9240167681a2cf0c6739f88dc2
pySoo <soopy368@gmail.com>
2024-03-18 02:38:26 +0900

    [🎨design]: 풀이 공유 페이지 디자인 수정

--- src/pages/ProblemSharePage/ProblemSharePage.style.ts
+++ src/pages/ProblemSharePage/ProblemSharePage.style.ts
@@ -1,16 +1,64 @@
 import styled from 'styled-components';
+import { css } from 'styled-components';
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  padding: 4rem 2rem;
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    padding: 4rem 2rem;
+    background-color: ${theme.color.background_editor};
+  `}
 `;
 
 const CodeEditorWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100%;
   margin-top: 2rem;
 `;
 
-export { CodeEditorWrapper, Wrapper };
+const SolveStatusWrapper = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: start;
+    font-weight: ${theme.fontWeight.semiBold};
+  `}
+`;
+
+const SolveSuccessText = styled.span`
+  ${({ theme }) => css`
+    padding: 1rem 1.2rem;
+    background-color: ${theme.color.green};
+    border-radius: ${theme.shape.round};
+  `}
+`;
+
+const SolveFailText = styled.span`
+  ${({ theme }) => css`
+    padding: 1rem 1.2rem;
+    background-color: ${theme.color.red};
+    border-radius: ${theme.shape.round};
+  `}
+`;
+
+const NoResultText = styled.span`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: center;
+    margin-top: 5rem;
+    font-size: ${theme.size.L};
+    font-weight: ${theme.fontWeight.semiBold};
+    color: ${theme.color.gray_50};
+  `}
+`;
+
+export {
+  CodeEditorWrapper,
+  NoResultText,
+  SolveFailText,
+  SolveStatusWrapper,
+  SolveSuccessText,
+  Wrapper,
+};
