5f652215d3ee4e082db18a44b2c64b06d1e488d8
타래 <85001878+meowTarae@users.noreply.github.com>
2024-03-19 18:00:59 +0900

    [✨feat]: PSHeader 방 나가기 버튼에 퇴장 함수 연동
    Co-authored-by: joyswim <joyswim.dev@gmail.com>

--- src/components/Common/Header/PSHeader.tsx
+++ src/components/Common/Header/PSHeader.tsx
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import { SOCKET_TYPE } from '@/constants/socket';
 import { useCustomTheme } from '@/hooks/useCustomTheme';
@@ -6,6 +6,7 @@ import useModal from '@/hooks/useModal';
 import ProblemTimer from '@/pages/ProblemSolvePage/ProblemTimer/ProblemTimer';
 import { PATH } from '@/routes/path';
 import useMessageStore from '@/store/MessageStore';
+import useRoomStore from '@/store/RoomStore';
 
 import Button from '../Button/Button';
 import Modal from '../Modal/Modal';
@@ -16,26 +17,28 @@ export default function PSHeader() {
   const { theme } = useCustomTheme();
   const { modalRef, isOpen, openModal, closeModal } = useModal();
   const location = useLocation();
-  const params = useParams();
-  const { roomShortUuid } = params;
-  const { sendMessage } = useMessageStore();
+  const { reset: resetRoom } = useRoomStore();
+
+  const { connected, sendMessage, disconnect } = useMessageStore();
 
   const isProblemSolvePage =
     location.pathname.split('/')[1] === PATH.PROBLEMSOLVE.replace('/', '');
 
   const navigate = useNavigate();
 
-  const navigateToHome = () => {
-    navigate(PATH.HOME);
-  };
-
   const handleGiveUp = () => {
     openModal();
   };
 
-  const handleNavigateToRoom = () => {
-    sendMessage(SOCKET_TYPE.ROOM.END_CODING);
-    navigate(`${PATH.ROOM}/${roomShortUuid}`, { replace: true });
+  const handleNavigateToHome = () => {
+    // 홈으로 나가면 CheckRoute에 의해 소켓 연결 disconnect
+    if (connected) {
+      sendMessage(SOCKET_TYPE.CHAT.QUIT);
+      disconnect();
+    }
+
+    resetRoom();
+    navigate(`${PATH.HOME}`, { replace: true });
   };
 
   return (
@@ -51,7 +54,7 @@ export default function PSHeader() {
           fontSize="1.4rem"
           backgroundColor={theme.color.gray_20}
           textColor={theme.color.gray_50}
-          onClick={isProblemSolvePage ? handleGiveUp : handleNavigateToRoom}
+          onClick={isProblemSolvePage ? handleGiveUp : handleNavigateToHome}
         >
           {isProblemSolvePage ? '포기하기' : '리뷰 종료'}
         </Button>
@@ -72,7 +75,7 @@ export default function PSHeader() {
               fontSize={theme.size.M}
               textColor={theme.color.white_primary}
               backgroundColor={theme.color.red}
-              onClick={navigateToHome}
+              onClick={handleNavigateToHome}
             >
               나가기
             </Button>
