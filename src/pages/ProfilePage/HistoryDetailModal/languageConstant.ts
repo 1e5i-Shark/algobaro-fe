744d46fecedbe7d5241515677e648c457d5bab73
Cho-Ik-Jun <harry960629@naver.com>
2024-03-19 17:42:26 +0900

    [✨feat]: 언어 데이터 변환 객체 추가

---
+++ src/pages/ProfilePage/HistoryDetailModal/languageConstant.ts
@@ -0,0 +1,8 @@
+import { LanguagesType } from '@/types/room';
+
+export const languageConvert: { [key: string]: LanguagesType } = {
+  javascript: 'JAVASCRIPT',
+  java: 'JAVA',
+  python: 'PYTHON',
+  cpp: 'C++',
+};
