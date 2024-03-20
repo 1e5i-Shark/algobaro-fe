727adef885a31c114b815cb853aa9af2bac2adae
pySoo <soopy368@gmail.com>
2024-03-20 12:58:40 +0900

    [🎨design]: 스타일링 수정
    Co-authored-by: Cho_Ik_Jun <harry960629@naver.com>

--- src/components/IDE/CodeEditor/utils.ts
+++ src/components/IDE/CodeEditor/utils.ts
@@ -60,6 +60,5 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
 const a = parseInt(input[0]);
 const b = parseInt(input[1]);
-console.log(a + b);
-`,
+console.log(a + b);`,
 };
