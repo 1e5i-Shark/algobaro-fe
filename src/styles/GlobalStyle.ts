eecfb8c855b82ea345895d3f949811d1ec16221b
suyoung <joyswim.dev@gmail.com>
2024-03-21 18:53:47 +0900

    [🎨design]: GlobalStyle 폰트 적용

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -21,6 +21,7 @@ export const GlobalStyle = createGlobalStyle`
   }
   
   body {
+    font-family: "Pretendard Variable", "Apple SD Gothic Neo", "Noto Sans KR", sans-serif;
     font-size: 1.6rem;
     color: ${({ theme }) => theme.color.text_primary_color};
     background-color: ${({ theme }) => theme.color.background_primary};
