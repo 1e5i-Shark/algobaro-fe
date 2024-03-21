e96f5c30bddd54b909c26ce942f2917c2ee904cd
Cho-Ik-Jun <harry960629@naver.com>
2024-03-21 12:04:31 +0900

    [✨feat]: 테스트 케이스 결과 API 함수 및 useMutation 커스텀 훅 구현

--- src/hooks/useProblemSolve.ts
+++ src/hooks/useProblemSolve.ts
@@ -5,6 +5,7 @@ import { SOCKET_TYPE } from '@/constants/socket';
 import { PATH } from '@/routes/path';
 import { compile } from '@/services/ProblemSolve/compile';
 import { submission } from '@/services/ProblemSolve/submission';
+import { testCaseSubmission } from '@/services/ProblemSolve/testCaseSubmission';
 import useCodeEditorStore from '@/store/CodeEditorStore';
 import useMessageStore from '@/store/MessageStore';
 import useRoomStore from '@/store/RoomStore';
@@ -35,6 +36,22 @@ export const useSubmission = () => {
       // 코드 제출 성공 시 소켓 END_CODING 전송 -> 공유 페이지로 이동
       sendMessage(SOCKET_TYPE.ROOM.END_CODING);
       navigate(`${PATH.PROBLEMSHARE}/${roomShortUuid}`, { replace: true });
+    },
+  });
+};
+
+/**
+ * 백준 문제 링크의 테스트 케이스에 대한 코드 결과를 가져오는 useMutation 훅
+ * @returns 테스트 결과 배열 객체
+ */
+export const useTestCaseSubmission = () => {
+  return useMutation({
+    mutationFn: testCaseSubmission,
+    onSuccess: data => {
+      if (data.response.testCaseResults) {
+        return data.response.testCaseResults;
+      }
+      return data;
     },
   });
 };
