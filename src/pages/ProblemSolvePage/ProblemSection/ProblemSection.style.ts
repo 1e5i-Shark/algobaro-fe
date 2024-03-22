810037ace08d812691d4b70e81d7a55f3dda668b
pySoo <soopy368@gmail.com>
2024-03-22 21:03:10 +0900

    [🎨design]: 문제 영역 반응형 fontSize 조정

--- src/pages/ProblemSolvePage/ProblemSection/ProblemSection.style.ts
+++ src/pages/ProblemSolvePage/ProblemSection/ProblemSection.style.ts
@@ -4,11 +4,19 @@ import { Col } from '@/styles/GlobalStyle';
 
 const ProblemWrapper = styled(Col)`
   ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
     height: calc(100% - 3rem);
     padding: 2rem;
     overflow-x: hidden;
     overflow-y: scroll;
+    font-size: ${theme.size.L};
     background-color: ${theme.color.background_editor};
+
+    @media (${theme.device.laptop}) {
+      font-size: 1.6rem;
+    }
   `}
 `;
 
@@ -20,20 +28,29 @@ const ProblemTextWrapper = styled.li`
 `;
 
 const ProblemContent = styled.div`
-  padding: 1rem 0 2rem;
-  line-height: 2.5rem;
+  padding: 2rem 0;
+  line-height: 3rem;
   ul {
     line-height: 1.5rem;
   }
   li {
     list-style: inside;
   }
+
+  @media (${({ theme }) => theme.device.laptop}) {
+    padding: 1rem 0 2rem;
+    line-height: 2.5rem;
+  }
 `;
 
 const ProblemTitle = styled.span`
   ${({ theme }) => css`
-    font-size: ${theme.size.L};
+    font-size: ${theme.size.XL};
     font-weight: ${theme.fontWeight.semiBold};
+
+    @media (${theme.device.laptop}) {
+      font-size: ${theme.size.L};
+    }
   `}
 `;
 
