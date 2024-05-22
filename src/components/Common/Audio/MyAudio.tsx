3a99b8904a628298305234ad0100c55678025b61
pySoo <soopy368@gmail.com>
2024-05-22 17:44:18 +0900

    [✨feat]: MyAudio는 장치 권한 받아오기, Socket 연결, 아이콘 클릭 시 active 상태 변경 등의 기능 처리

---
+++ src/components/Common/Audio/MyAudio.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react';
+
+import useAudioStore from '@/store/AudioStore';
+import useRoomStore from '@/store/RoomStore';
+
+import Tooltip from '../Tooltip/Tooltip';
+import Audio from './Audio';
+
+interface MyAudioProps {
+  memberId: number;
+}
+
+export default function MyAudio({ memberId }: MyAudioProps) {
+  const key = memberId.toString();
+
+  const [audioStream, setAudioStream] = useState<MediaStream | null>(null);
+  const [isActive, setIsActive] = useState(false);
+
+  const { connect, disconnect, connected, otherKeyList, sendOffer } =
+    useAudioStore();
+
+  const {
+    roomData: { roomShortUuid },
+  } = useRoomStore();
+
+  const openMediaDevices = async () => {
+    console.log('socket flow: 1. getUserMedia 나는', memberId, '번 유저');
+    return await navigator.mediaDevices.getUserMedia({ audio: true });
+  };
+
+  const startAudio = async () => {
+    try {
+      const stream = await openMediaDevices();
+      // 진입 시 음소거 설정
+      stream.getAudioTracks()[0].enabled = false;
+      setAudioStream(stream);
+
+      connect(key, stream, roomShortUuid);
+    } catch (error) {
+      console.error('Error accessing media devices.', error);
+    }
+  };
+
+  const handleIconClick = () => {
+    if (audioStream === null) {
+      startAudio();
+    }
+
+    if (audioStream === null) return;
+
+    const track = audioStream.getAudioTracks()[0];
+    track.enabled = !track.enabled;
+
+    setIsActive(track.enabled);
+  };
+
+  useEffect(() => {
+    startAudio();
+
+    return () => {
+      disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (connected && otherKeyList.length > 0) {
+      sendOffer(key);
+    }
+  }, [connected, otherKeyList]);
+
+  return (
+    <Tooltip
+      text={audioStream?.getTracks()[0].label ?? '선택된 마이크가 없습니다'}
+    >
+      <div>
+        <Audio
+          isMyAudio
+          audioStream={audioStream}
+          isActive={isActive}
+          onIconClick={handleIconClick}
+        />
+      </div>
+    </Tooltip>
+  );
+}
