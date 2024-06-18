2e59a83abf376a0e56be7ce3051c3d9be9a12215
Suyoung Shin <joyswim.dev@gmail.com>
2024-06-18 22:59:26 +0900

    [✨feat]: 오디오 리스트 렌더링 및 오디오 기기 변경 로직 추가

--- src/components/Common/Audio/MyAudio.tsx
+++ src/components/Common/Audio/MyAudio.tsx
@@ -3,8 +3,10 @@ import { useEffect, useState } from 'react';
 import { useAudioSocket } from '@/hooks/Audio/useAudioSocket';
 import useRoomStore from '@/store/RoomStore';
 
-import Tooltip from '../Tooltip/Tooltip';
 import Audio from './Audio';
+import AudioMenu from './AudioMenu';
+import { MyAudioWrapper } from './MyAudio.style';
+import { Tooltip } from '@/components';
 
 interface MyAudioProps {
   memberId: number;
@@ -14,21 +16,35 @@ export default function MyAudio({ memberId }: MyAudioProps) {
   const key = memberId.toString();
 
   const [audioStream, setAudioStream] = useState<MediaStream | null>(null);
+  const [audioDevices, setAudioDevices] = useState<MediaDeviceInfo[] | null>(
+    null
+  );
   const [isActive, setIsActive] = useState(true);
 
   const {
     roomData: { roomShortUuid },
   } = useRoomStore();
 
-  const { client, connectSocket, disconnectSocket } = useAudioSocket({
-    myKey: key,
-    roomShortUuid,
-  });
+  const { client, connectSocket, disconnectSocket, replaceTrack } =
+    useAudioSocket({
+      myKey: key,
+      roomShortUuid,
+    });
 
-  const openMediaDevices = async () => {
+  const getAudioDevices = async () => {
+    return await navigator.mediaDevices.enumerateDevices();
+  };
+
+  const openMediaDevices = async (deviceId?: string) => {
     console.log('socket flow: 1. getUserMedia 나는', memberId, '번 유저');
     return await navigator.mediaDevices.getUserMedia({
-      audio: true,
+      audio: deviceId
+        ? {
+            deviceId: {
+              exact: deviceId,
+            },
+          }
+        : true,
     });
   };
 
@@ -39,6 +55,15 @@ export default function MyAudio({ memberId }: MyAudioProps) {
       // 진입 시 음소거 설정
       stream.getAudioTracks()[0].enabled = true;
       setAudioStream(stream);
+
+      const devices = await getAudioDevices();
+      const audioList = devices.filter(
+        device =>
+          device.kind === 'audioinput' && device.deviceId !== 'communications'
+      );
+
+      setAudioDevices(audioList);
+
       // socket 연결
       console.log('---1. connect ---', stream);
       connectSocket(stream);
@@ -71,20 +96,40 @@ export default function MyAudio({ memberId }: MyAudioProps) {
     };
   }, []);
 
+  const handleChangeDevice = async (deviceId: string) => {
+    try {
+      const stream = await openMediaDevices(deviceId);
+      replaceTrack(stream);
+    } catch (error) {
+      console.error('media 변경에 실패했습니다.', error);
+    }
+  };
+
   return (
-    <Tooltip
-      text={audioStream?.getTracks()[0].label ?? '선택된 마이크가 없습니다'}
-    >
-      <div>
-        <Audio
-          memberId={key}
-          isMyAudio
+    <MyAudioWrapper>
+      <Tooltip
+        text={
+          audioStream?.getTracks()[0].label ? '' : '선택된 마이크가 없습니다'
+        }
+      >
+        <div>
+          <Audio
+            memberId={key}
+            isMyAudio
+            audioStream={audioStream}
+            isActive={isActive}
+            onIconClick={handleIconClick}
+            connectSocket={connectSocket}
+          />
+        </div>
+      </Tooltip>
+      {audioDevices && audioStream && (
+        <AudioMenu
           audioStream={audioStream}
-          isActive={isActive}
-          onIconClick={handleIconClick}
-          connectSocket={connectSocket}
+          audioDevices={audioDevices}
+          onChange={handleChangeDevice}
         />
-      </div>
-    </Tooltip>
+      )}
+    </MyAudioWrapper>
   );
 }
